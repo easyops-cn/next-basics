2c6ceb0093b50db2753fc3e2a751260530930c8c
weareoutman <wangshenwei@qq.com>
2021-04-09 10:29:01 +0800

    feat(): get depending contracts by provider bricks

--- bricks/next-builder/src/data-providers/ScanBricksAndTemplates.ts
+++ bricks/next-builder/src/data-providers/ScanBricksAndTemplates.ts
@@ -1,4 +1,4 @@
-import { paramCase } from "param-case";
+import { paramCase, snakeCase, pascalCase } from "change-case";
 import { uniq } from "lodash";
 import {
   createProviderClass,
@@ -20,6 +20,12 @@ interface ScanBricksAndTemplatesResult {
   templates: string[];
   processors: string[];
   processorPackages: string[];
+  contracts: ContractDeclaration[];
+}
+
+interface ContractDeclaration {
+  contract: string;
+  version: string;
 }
 
 export function ScanBricksAndTemplates({
@@ -27,6 +33,20 @@ export function ScanBricksAndTemplates({
 }: ScanBricksAndTemplatesParams): ScanBricksAndTemplatesResult {
   const processors = scanProcessorsInStoryboard(storyboard);
   const { bricks, customApis } = scanStoryboard(storyboard);
+  const contracts: ContractDeclaration[] = [];
+
+  for (const brick of bricks) {
+    const match = brick.match(/^providers-of-([^.]+)\.(.+?)-api-(.+)$/);
+    if (match) {
+      contracts.push({
+        contract: `easyops.api.${snakeCase(match[1])}.${snakeCase(
+          match[2]
+        )}.${pascalCase(match[3])}`,
+        version: "*",
+      });
+    }
+  }
+
   return {
     apis: mapCustomApisToNameAndNamespace(customApis),
     bricks,
@@ -36,6 +56,7 @@ export function ScanBricksAndTemplates({
       // The package name should always be the param-case of processor's namespace.
       processors.map((item) => paramCase(item.split(".")[0]))
     ),
+    contracts,
   };
 }
 
