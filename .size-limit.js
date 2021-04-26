e655ed63061aaf67a976254a7c8e977ab468cd92
weareoutman <wangshenwei@qq.com>
2021-04-26 17:32:49 +0800

    chore(): increase size-limit which uses 1000 as KB

--- .size-limit.js
+++ .size-limit.js
@@ -3,12 +3,12 @@ const { sizeLimit } = require("@next-core/build-config-factory");
 module.exports = sizeLimit({
   bricks: {
     "*": "50 KB",
-    "basic-bricks": "110 KB",
+    "basic-bricks": "115 KB",
     "presentational-bricks": "350 KB",
     "brick-visualization": "430 KB",
     developers: "260 KB",
     forms: "225 KB",
-    "next-builder": "325 KB",
+    "next-builder": "330 KB",
   },
   templates: {
     "*": "5 KB",
