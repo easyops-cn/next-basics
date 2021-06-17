fa3064c27018cdb9dd3f5d1920ec231c3a39f96b
weareoutman <wangshenwei@qq.com>
2021-06-17 16:34:35 +0800

    feat(): support tp load hosted snippets

--- bricks/next-builder/src/builder-container/BrickLibrary/groupBricks.ts
+++ bricks/next-builder/src/builder-container/BrickLibrary/groupBricks.ts
@@ -8,7 +8,10 @@ export function groupBricks(
   return Object.entries(
     groupBy(
       brickList.map((brick) => {
-        if (brick.type === "customTemplate" && !brick.name.includes(".")) {
+        if (
+          ["customTemplate", "snippet"].includes(brick.type) &&
+          !brick.name.includes(".")
+        ) {
           // For a custom template defined in a micro-app,
           // it has no scope name in its brick name.
           return {
--- bricks/next-builder/src/data-providers/GetBrickLibrary.ts
+++ bricks/next-builder/src/data-providers/GetBrickLibrary.ts
@@ -1,19 +1,64 @@
-import { developHelper } from "@next-core/brick-kit";
-import { createProviderClass } from "@next-core/brick-utils";
-import { InstanceApi_postSearch } from "@next-sdk/cmdb-sdk";
+import { developHelper, i18nText } from "@next-core/brick-kit";
+import { BrickConf } from "@next-core/brick-types";
+import { createProviderClass, pipes } from "@next-core/brick-utils";
+import {
+  InstanceApi_postSearchV3,
+  InstanceGraphApi_traverseGraphV2,
+} from "@next-sdk/cmdb-sdk";
+import { buildBricks } from "../shared/storyboard/buildStoryboard";
 
 export interface BrickLibraryItem {
-  type: "brick" | "template" | "customTemplate" | "provider";
+  type: "brick" | "template" | "customTemplate" | "provider" | "snippet";
   name: string;
+  isHostedSnippets?: boolean;
+  bricks?: BrickConf[];
+  category?: string;
+  thumbnail?: string;
 }
 
 export interface GetBrickLibraryParams {
+  // Project instanceId.
   projectId: string;
 }
 
 export async function GetBrickLibrary({
   projectId,
 }: GetBrickLibraryParams): Promise<BrickLibraryItem[]> {
+  const [customTemplates, installedSnippets, hostedSnippets] =
+    await Promise.all([
+      InstanceApi_postSearchV3("STORYBOARD_TEMPLATE", {
+        fields: ["templateId", "id"],
+        page_size: 3000,
+        query: {
+          "project.instanceId": projectId,
+        },
+      }),
+      InstanceApi_postSearchV3("INSTALLED_BRICK_SNIPPET@EASYOPS", {
+        fields: ["id", "text", "category", "thumbnail", "bricks"],
+        page_size: 3000,
+      }),
+      InstanceGraphApi_traverseGraphV2({
+        child: [
+          {
+            child: [
+              {
+                depth: -1,
+                parentOut: "children",
+                select_fields: ["*"],
+              },
+            ],
+            depth: -1,
+            parentOut: "children",
+            select_fields: ["*"],
+          },
+        ],
+        object_id: "STORYBOARD_SNIPPET",
+        query: {
+          "project.instanceId": projectId,
+        },
+        select_fields: ["*"],
+      }),
+    ]);
   return developHelper
     .getBrickPackages()
     .flatMap<BrickLibraryItem>((pkg) =>
@@ -25,26 +70,33 @@ export async function GetBrickLibrary({
           }))
     )
     .concat(
-      (
-        await InstanceApi_postSearch("STORYBOARD_TEMPLATE", {
-          fields: {
-            templateId: 1,
-            id: 1,
-          },
-
-          page: 1,
-          page_size: 3000,
-          query: {
-            "project.instanceId": projectId,
-          },
-        })
-      ).list.map<BrickLibraryItem>((item) => ({
+      customTemplates.list.map<BrickLibraryItem>((item) => ({
         type: "customTemplate",
         name: item.templateId,
         id: item.id,
-      }))
-    )
-    .concat(
+      })),
+      installedSnippets.list.map<BrickLibraryItem>((item) => ({
+        type: "snippet",
+        name: i18nText(item.text) || item.id,
+        category: item.category,
+        thumbnail: item.thumbnail,
+        bricks: item.bricks,
+      })),
+      pipes
+        .graphTree(hostedSnippets as pipes.GraphData, {
+          sort: {
+            key: "sort",
+            order: 1,
+          },
+        })
+        .map<BrickLibraryItem>((item) => ({
+          type: "snippet",
+          name: i18nText(item.text) || item.snippetId,
+          isHostedSnippets: true,
+          category: item.category,
+          thumbnail: item.thumbnail,
+          bricks: buildBricks(item.children),
+        })),
       developHelper.getTemplatePackages().flatMap<BrickLibraryItem>((pkg) =>
         pkg.templates.map((name) => ({
           type: "template",
