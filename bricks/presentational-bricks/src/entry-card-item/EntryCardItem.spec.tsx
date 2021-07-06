77a901312e565bbc6b8481d7349a5ae291171d3a
nlicroshan <nlicroshan@easyops.cn>
2021-07-06 11:33:22 +0800

    feat(entry-card-item): add some style property
    Closes OS-39

--- bricks/presentational-bricks/src/entry-card-item/EntryCardItem.spec.tsx
+++ bricks/presentational-bricks/src/entry-card-item/EntryCardItem.spec.tsx
@@ -7,7 +7,9 @@ describe("EntryCardItem", () => {
     const wrapper = shallow(
       <EntryCardItem
         cardTitle="卡片标题"
+        description="卡片描述"
         iconColor="brightOrange"
+        hoverHighLight={true}
         icon={{
           lib: "easyops",
           category: "model",
@@ -17,5 +19,12 @@ describe("EntryCardItem", () => {
       />
     );
     expect(wrapper.find("Link").length).toBe(1);
+    wrapper.setProps({
+      showCard: true,
+      target: "_blank",
+      url: undefined,
+    });
+    wrapper.update();
+    expect(wrapper.find("Card").length).toBe(1);
   });
 });
