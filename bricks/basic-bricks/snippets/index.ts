6039dc8aca3eacc649f84759ac96f607e25ac171
qimengwu <qimengwu@easyops.cn>
2021-07-15 12:00:49 +0800

    feat(easy-view): add some snippets to easy-view
    Refs#NEXT_BUILDER-876

--- bricks/basic-bricks/snippets/index.ts
+++ bricks/basic-bricks/snippets/index.ts
@@ -1,4 +1,12 @@
-import easyViewSnippets from "./easy-view";
+//import easyViewSnippets from "./easy-view";
+import bannertLayoutSnippets from "./layout/banner-layout";
+import classicLayoutSnippets from "./layout/classic-layout";
+import menuLayoutSnippets from "./layout/menu-layout";
 import generalButtonSnippets from "./general-button";
 
-export const snippets = [...easyViewSnippets, ...generalButtonSnippets];
+export const snippets = [
+  ...bannertLayoutSnippets,
+  ...classicLayoutSnippets,
+  ...menuLayoutSnippets,
+  ...generalButtonSnippets,
+];
