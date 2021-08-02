cf284f76947de38c83f85d5782b1b3556c182c84
weareoutman <wangshenwei@qq.com>
2021-08-02 16:28:18 +0800

    fix(): make some bricks lazy load

--- .size-limit.js
+++ .size-limit.js
@@ -6,13 +6,13 @@ module.exports = sizeLimit({
     "basic-bricks": "125 KB",
     "presentational-bricks": "90 KB",
     "brick-visualization": "430 KB",
-    developers: "205 KB",
     forms: "225 KB",
   },
   lazyBricks: {
     "*": "50 KB",
     "next-builder": "325 KB",
     "presentational-bricks": "230 KB",
+    developers: "170 KB",
   },
   templates: {
     "*": "5 KB",
