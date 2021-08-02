f77c502fdd341c6ec8582949ddf84919a51628a3
qimengwu <qimengwu@easyops.cn>
2021-08-02 15:36:07 +0800

    feat(easy-view): add some snipeets to easy-view
    Refs#NEXT_BUILDER-876

--- bricks/basic-bricks/snippets/index.ts
+++ bricks/basic-bricks/snippets/index.ts
@@ -1,12 +1,10 @@
 //import easyViewSnippets from "./easy-view";
 import bannertLayoutSnippets from "./layout/banner-layout";
 import classicLayoutSnippets from "./layout/classic-layout";
-import menuLayoutSnippets from "./layout/menu-layout";
 import generalButtonSnippets from "./general-button";
 
 export const snippets = [
   ...bannertLayoutSnippets,
   ...classicLayoutSnippets,
-  ...menuLayoutSnippets,
   ...generalButtonSnippets,
 ];
--- bricks/basic-bricks/snippets/layout/banner-layout.ts
+++ bricks/basic-bricks/snippets/layout/banner-layout.ts
@@ -3,9 +3,15 @@ import { SnippetDefinition } from "@next-core/brick-types";
 
 import banner1_2Svg from "../thumbnails/banner-layout/grid-banner-left1-right2.svg";
 import banner1_3Svg from "../thumbnails/banner-layout/grid-banner-left1-right3.svg";
+import banner1_3SecondSvg from "../thumbnails/banner-layout/grid-banner-left1-right3-2.svg";
 import banner2_1Svg from "../thumbnails/banner-layout/grid-banner-left2-right1.svg";
 import banner3_1Svg from "../thumbnails/banner-layout/grid-banner-left3-right1.svg";
+import banner3_1SecondSvg from "../thumbnails/banner-layout/grid-banner-left3-right1-2.svg";
 import bannerOneColSvg from "../thumbnails/banner-layout/grid-banner-one-col.svg";
+
+import bannerRightFirstSvg from "../thumbnails/banner-layout/grid-banner-right-1.svg";
+import bannerRightSecondSvg from "../thumbnails/banner-layout/grid-banner-right-2.svg";
+import bannerRightThirdSvg from "../thumbnails/banner-layout/grid-banner-right-3.svg";
 
 const snippets: SnippetDefinition[] = [
   {
@@ -22,16 +28,29 @@ const snippets: SnippetDefinition[] = [
         brick: "basic-bricks.easy-view",
         properties: {
           containerStyle: {
-            padding: "var(--page-padding)",
             minHeight: "calc(100vh - var(--app-bar-height))",
-            gap: "var(--card-content-gap)",
           },
-          gridTemplateAreas: [
-            ["banner", "banner"],
-            ["contentLeft", "contentRight"],
-          ],
-          gridTemplateRows: ["auto", "1fr"],
-          gridTemplateColumns: ["1fr", "2fr"],
+          gridTemplateAreas: [["banner"], ["content"]],
+          gridTemplateRows: ["260px", "1fr"],
+        },
+        slots: {
+          content: {
+            bricks: [
+              {
+                brick: "basic-bricks.easy-view",
+                properties: {
+                  containerStyle: {
+                    padding: "16px",
+                    minHeight: "calc(100vh - 260px - var(--app-bar-height))",
+                    gap: "16px",
+                  },
+                  gridTemplateAreas: [["contentLeft", "contentRight"]],
+                  gridTemplateColumns: ["1fr", "2fr"],
+                },
+              },
+            ],
+            type: "bricks",
+          },
         },
       },
     ],
@@ -50,16 +69,74 @@ const snippets: SnippetDefinition[] = [
         brick: "basic-bricks.easy-view",
         properties: {
           containerStyle: {
-            padding: "var(--page-padding)",
             minHeight: "calc(100vh - var(--app-bar-height))",
-            gap: "var(--card-content-gap)",
           },
-          gridTemplateAreas: [
-            ["banner", "banner"],
-            ["contentLeft", "contentRight"],
-          ],
-          gridTemplateRows: ["auto", "1fr"],
-          gridTemplateColumns: ["1fr", "3fr"],
+          gridTemplateAreas: [["banner"], ["content"]],
+          gridTemplateRows: ["260px", "1fr"],
+        },
+        slots: {
+          content: {
+            bricks: [
+              {
+                brick: "basic-bricks.easy-view",
+                properties: {
+                  containerStyle: {
+                    padding: "16px",
+                    minHeight: "calc(100vh - 260px - var(--app-bar-height))",
+                    gap: "16px",
+                  },
+                  gridTemplateAreas: [["contentLeft", "contentRight"]],
+                  gridTemplateColumns: ["1fr", "3fr"],
+                },
+              },
+            ],
+            type: "bricks",
+          },
+        },
+      },
+    ],
+  },
+  {
+    id: "basic-bricks.easy-view[banner-1:3-2]",
+    layerType: "layout",
+    category: "banner",
+    text: {
+      zh: "1:3-2 通栏布局",
+      en: "1:3-2 banner layout",
+    },
+    thumbnail: banner1_3SecondSvg,
+    bricks: [
+      {
+        brick: "basic-bricks.easy-view",
+        properties: {
+          containerStyle: {
+            minHeight: "calc(100vh - var(--app-bar-height))",
+          },
+          gridTemplateAreas: [["banner"], ["content"]],
+          gridTemplateRows: ["260px", "1fr"],
+        },
+        slots: {
+          content: {
+            bricks: [
+              {
+                brick: "basic-bricks.easy-view",
+                properties: {
+                  containerStyle: {
+                    padding: "16px",
+                    minHeight: "calc(100vh - 260px - var(--app-bar-height))",
+                    gap: "16px",
+                  },
+                  gridTemplateAreas: [
+                    ["contentLeft", "contentRightTop"],
+                    ["contentLeft", "contentRightBottom"],
+                  ],
+                  gridTemplateColumns: ["1fr", "3fr"],
+                  gridTemplateRows: ["1fr", "1fr"],
+                },
+              },
+            ],
+            type: "bricks",
+          },
         },
       },
     ],
@@ -78,16 +155,29 @@ const snippets: SnippetDefinition[] = [
         brick: "basic-bricks.easy-view",
         properties: {
           containerStyle: {
-            padding: "var(--page-padding)",
             minHeight: "calc(100vh - var(--app-bar-height))",
-            gap: "var(--card-content-gap)",
           },
-          gridTemplateAreas: [
-            ["banner", "banner"],
-            ["contentLeft", "contentRight"],
-          ],
-          gridTemplateRows: ["auto", "1fr"],
-          gridTemplateColumns: ["2fr", "1fr"],
+          gridTemplateAreas: [["banner"], ["content"]],
+          gridTemplateRows: ["260px", "1fr"],
+        },
+        slots: {
+          content: {
+            bricks: [
+              {
+                brick: "basic-bricks.easy-view",
+                properties: {
+                  containerStyle: {
+                    padding: "16px",
+                    minHeight: "calc(100vh - 260px - var(--app-bar-height))",
+                    gap: "16px",
+                  },
+                  gridTemplateAreas: [["contentLeft", "contentRight"]],
+                  gridTemplateColumns: ["2fr", "1fr"],
+                },
+              },
+            ],
+            type: "bricks",
+          },
         },
       },
     ],
@@ -106,16 +196,74 @@ const snippets: SnippetDefinition[] = [
         brick: "basic-bricks.easy-view",
         properties: {
           containerStyle: {
-            padding: "var(--page-padding)",
             minHeight: "calc(100vh - var(--app-bar-height))",
-            gap: "var(--card-content-gap)",
           },
-          gridTemplateAreas: [
-            ["banner", "banner"],
-            ["contentLeft", "contentRight"],
-          ],
-          gridTemplateRows: ["auto", "1fr"],
-          gridTemplateColumns: ["3fr", "1fr"],
+          gridTemplateAreas: [["banner"], ["content"]],
+          gridTemplateRows: ["260px", "1fr"],
+        },
+        slots: {
+          content: {
+            bricks: [
+              {
+                brick: "basic-bricks.easy-view",
+                properties: {
+                  containerStyle: {
+                    padding: "16px",
+                    minHeight: "calc(100vh - 260px - var(--app-bar-height))",
+                    gap: "16px",
+                  },
+                  gridTemplateAreas: [["contentLeft", "contentRight"]],
+                  gridTemplateColumns: ["3fr", "1fr"],
+                },
+              },
+            ],
+            type: "bricks",
+          },
+        },
+      },
+    ],
+  },
+  {
+    id: "basic-bricks.easy-view[banner-3:1-2]",
+    layerType: "layout",
+    category: "banner",
+    text: {
+      zh: "3:1-2 通栏布局",
+      en: "3:1-2 banner layout",
+    },
+    thumbnail: banner3_1SecondSvg,
+    bricks: [
+      {
+        brick: "basic-bricks.easy-view",
+        properties: {
+          containerStyle: {
+            minHeight: "calc(100vh - var(--app-bar-height))",
+          },
+          gridTemplateAreas: [["banner"], ["content"]],
+          gridTemplateRows: ["260px", "1fr"],
+        },
+        slots: {
+          content: {
+            bricks: [
+              {
+                brick: "basic-bricks.easy-view",
+                properties: {
+                  containerStyle: {
+                    padding: "16px",
+                    minHeight: "calc(100vh - 260px - var(--app-bar-height))",
+                    gap: "16px",
+                  },
+                  gridTemplateAreas: [
+                    ["contentLeftTop", "contentRight"],
+                    ["contentLeftBottom", "contentRight"],
+                  ],
+                  gridTemplateColumns: ["3fr", "1fr"],
+                  gridTemplateRows: ["1fr", "1fr"],
+                },
+              },
+            ],
+            type: "bricks",
+          },
         },
       },
     ],
@@ -134,12 +282,128 @@ const snippets: SnippetDefinition[] = [
         brick: "basic-bricks.easy-view",
         properties: {
           containerStyle: {
-            padding: "var(--page-padding)",
             minHeight: "calc(100vh - var(--app-bar-height))",
-            gap: "var(--card-content-gap)",
           },
           gridTemplateAreas: [["banner"], ["content"]],
-          gridTemplateRows: ["auto", "1fr"],
+          gridTemplateRows: ["260px", "1fr"],
+          styleByAreas: {
+            content: {
+              margin: "16px",
+            },
+          },
+        },
+      },
+    ],
+  },
+  {
+    id: "basic-bricks.easy-view[banner-right-1]",
+    layerType: "layout",
+    category: "banner",
+    text: {
+      zh: "右通栏布局1",
+      en: "Right banner layout 1",
+    },
+    thumbnail: bannerRightFirstSvg,
+    bricks: [
+      {
+        brick: "basic-bricks.easy-view",
+        properties: {
+          containerStyle: {
+            minHeight: "calc(100vh - var(--app-bar-height))",
+          },
+          gridTemplateAreas: [["content", "banner"]],
+          gridTemplateColumns: ["3fr", "1fr"],
+          styleByAreas: {
+            content: {
+              margin: "16px",
+            },
+          },
+        },
+      },
+    ],
+  },
+  {
+    id: "basic-bricks.easy-view[banner-right-2]",
+    layerType: "layout",
+    category: "banner",
+    text: {
+      zh: "右通栏布局2",
+      en: "Right banner layout 2",
+    },
+    thumbnail: bannerRightSecondSvg,
+    bricks: [
+      {
+        brick: "basic-bricks.easy-view",
+        properties: {
+          containerStyle: {
+            minHeight: "calc(100vh - var(--app-bar-height))",
+          },
+          gridTemplateAreas: [["content", "banner"]],
+          gridTemplateColumns: ["3fr", "1fr"],
+        },
+        slots: {
+          content: {
+            bricks: [
+              {
+                brick: "basic-bricks.easy-view",
+                properties: {
+                  containerStyle: {
+                    padding: "16px",
+                    minHeight: "calc(100vh - var(--app-bar-height))",
+                    gap: "16px",
+                  },
+                  gridTemplateAreas: [["contentTop"], ["contentBottom"]],
+                  gridTemplateRows: ["162px", "1fr"],
+                },
+              },
+            ],
+            type: "bricks",
+          },
+        },
+      },
+    ],
+  },
+  {
+    id: "basic-bricks.easy-view[banner-right-3]",
+    layerType: "layout",
+    category: "banner",
+    text: {
+      zh: "右通栏布局3",
+      en: "Right banner layout 3",
+    },
+    thumbnail: bannerRightThirdSvg,
+    bricks: [
+      {
+        brick: "basic-bricks.easy-view",
+        properties: {
+          containerStyle: {
+            minHeight: "calc(100vh - var(--app-bar-height))",
+          },
+          gridTemplateAreas: [["content", "banner"]],
+          gridTemplateColumns: ["3fr", "1fr"],
+        },
+        slots: {
+          content: {
+            bricks: [
+              {
+                brick: "basic-bricks.easy-view",
+                properties: {
+                  containerStyle: {
+                    padding: "16px",
+                    minHeight: "calc(100vh - var(--app-bar-height))",
+                    gap: "16px",
+                  },
+                  gridTemplateAreas: [
+                    ["contentTop"],
+                    ["contentCenter"],
+                    ["contentBottom"],
+                  ],
+                  gridTemplateRows: ["162px", "1fr", "1fr"],
+                },
+              },
+            ],
+            type: "bricks",
+          },
         },
       },
     ],
