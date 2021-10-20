a04dcfc9ab63d343cb75e462ae5189319c20e962
dophijing <dophijing@163.com>
2021-10-20 16:13:03 +0800

    feat(forms): struct form support inner.form.init Close PIPELINE-1433

--- bricks/forms/src/general-structs-form-item/GeneralStructsFormItem.spec.tsx
+++ bricks/forms/src/general-structs-form-item/GeneralStructsFormItem.spec.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import { GeneralStructsFormItem } from "./GeneralStructsFormItem";
 import { Table, Modal } from "antd";
 describe("GeneralStructsFormItem", () => {
@@ -9,25 +9,48 @@ describe("GeneralStructsFormItem", () => {
         {
           name: "param1",
           type: "string",
-          description: "参数说明1"
+          description: "参数说明1",
         },
         {
           name: "param2",
           type: "int",
-          description: "参数说明2"
-        }
+          description: "参数说明2",
+        },
       ],
       fieldsMap: {
         name: "参数名",
         type: "参数类型",
-        description: "参数说明"
-      }
+        description: "参数说明",
+      },
     };
     const wrapper = shallow(<GeneralStructsFormItem {...props} />);
-    expect(wrapper.find(Table).prop("columns")[0]).toEqual({
-      dataIndex: "name",
-      key: "name",
-      title: "参数名"
-    });
+    expect(wrapper.find(Table).prop("columns")[0]?.title).toEqual("参数名");
+  });
+
+  it("structItemShowRenderFN should work", () => {
+    const props = {
+      dataSource: [
+        {
+          name: "param1",
+          type: "string",
+          description: "参数说明1",
+        },
+        {
+          name: "param2",
+          type: "int",
+          description: "参数说明2",
+        },
+      ],
+      fieldsMap: {
+        name: "参数名",
+        type: "参数类型",
+        description: "参数说明",
+      },
+      structItemShowRenderFN: (text: string) => text + "***",
+    };
+    const wrapper = mount(<GeneralStructsFormItem {...props} />);
+    expect(wrapper.find(Table).prop("columns")[0]?.render("test")).toEqual(
+      "test***"
+    );
   });
 });
