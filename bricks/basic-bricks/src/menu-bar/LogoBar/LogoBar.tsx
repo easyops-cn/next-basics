dea170c3487b255daf4bfb6c40b3c89c0e6d285f
李佳明 <jimmyli@easyops.cn>
2021-10-30 13:13:13 +0800

    feat(LogoBar): logo支持不设置link refs BRICK_STORE-2313

--- bricks/basic-bricks/src/menu-bar/LogoBar/LogoBar.tsx
+++ bricks/basic-bricks/src/menu-bar/LogoBar/LogoBar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ReactComponentElement } from "react";
 import { Link } from "@next-libs/basic-components";
 import styles from "./LogoBar.module.css";
 import { ReactComponent as Logo } from "../../images/logo-3.1.svg";
@@ -6,18 +6,23 @@ import { getRuntime } from "@next-core/brick-kit";
 
 export function LogoBar(): React.ReactElement {
   const brand = getRuntime().getBrandSettings();
+  const logo: React.ReactElement = brand.menu_bar_logo_url ? (
+    <img
+      src={brand.menu_bar_logo_url}
+      style={{ height: 32, verticalAlign: "top" }}
+    />
+  ) : (
+    <Logo height={32} style={{ verticalAlign: "top" }} />
+  );
   return (
     <div className={styles.logoBar}>
-      <Link to="/" className={styles.logoLink}>
-        {brand.menu_bar_logo_url ? (
-          <img
-            src={brand.menu_bar_logo_url}
-            style={{ height: 32, verticalAlign: "top" }}
-          />
-        ) : (
-          <Logo height={32} style={{ verticalAlign: "top" }} />
-        )}
-      </Link>
+      {brand.menu_bar_logo_no_link === "true" ? (
+        logo
+      ) : (
+        <Link to="/" className={styles.logoLink}>
+          {logo}
+        </Link>
+      )}
     </div>
   );
 }
