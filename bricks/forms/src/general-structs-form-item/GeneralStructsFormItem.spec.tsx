2b5f0754101452e473da4f57c492d1a6b314daf5
dophijing <dophijing@163.com>
2021-11-18 17:57:52 +0800

    feat(forms): general struce support structInnerTableColumnsOrder close PIPELINE-1438

--- bricks/forms/src/general-structs-form-item/GeneralStructsFormItem.spec.tsx
+++ bricks/forms/src/general-structs-form-item/GeneralStructsFormItem.spec.tsx
@@ -53,4 +53,29 @@ describe("GeneralStructsFormItem", () => {
       "test***"
     );
   });
+
+  it("structInnerTableColumnsOrder should work", () => {
+    const props = {
+      dataSource: [
+        {
+          name: "param1",
+          type: "string",
+          description: "参数说明1",
+        },
+        {
+          name: "param2",
+          type: "int",
+          description: "参数说明2",
+        },
+      ],
+      fieldsMap: {
+        name: "参数名",
+        type: "参数类型",
+        description: "参数说明",
+      },
+      structInnerTableColumnsOrder: ["type", "name", "description"],
+    };
+    const wrapper = mount(<GeneralStructsFormItem {...props} />);
+    expect(wrapper.find(Table).prop("columns")[0].key).toEqual("type");
+  });
 });
