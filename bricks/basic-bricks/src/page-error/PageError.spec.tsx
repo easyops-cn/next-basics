86010dde107e0d67154400424a12d9831d9a1266
MJmajiong <936233505@qq.com>
2021-12-05 15:52:07 +0800

    refactor(): 修改单元测试

--- bricks/basic-bricks/src/page-error/PageError.spec.tsx
+++ bricks/basic-bricks/src/page-error/PageError.spec.tsx
@@ -4,19 +4,8 @@ import { PageError } from "./PageError";
 import { BrickIllustration } from "../../../presentational-bricks/src/brick-illustration/BrickIllustration";
 
 describe("PageError", () => {
-  it("should show http message", () => {
-    jest.mock("./PageError", () => {
-      return {
-        httpCodeObj: {},
-      };
-    });
-    const wrapper = mount(<PageError error="oops"></PageError>);
-    expect(wrapper.find(BrickIllustration).exists()).toBe(false);
-  });
   it("should show image", () => {
     const wrapper = mount(<PageError error="oops"></PageError>);
-    wrapper.setProps({});
-    expect(wrapper.find(BrickIllustration).exists()).toBe(false);
     wrapper.setProps({ code: "444" });
     expect(wrapper.find(BrickIllustration).exists()).toBe(false);
     wrapper.setProps({ code: "403" });
