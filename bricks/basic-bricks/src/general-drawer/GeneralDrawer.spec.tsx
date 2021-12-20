d8477b6fe5e2c9131ebf832e7d8705b054dcdc36
nlicroshan <nlicroshan@easyops.cn>
2021-12-20 16:17:32 +0800

    feat(basic-bricks): customSwitchConfig支持文字
    Closes MONITOR-2788

--- bricks/basic-bricks/src/general-drawer/GeneralDrawer.spec.tsx
+++ bricks/basic-bricks/src/general-drawer/GeneralDrawer.spec.tsx
@@ -24,4 +24,28 @@ describe("GeneralModal", () => {
     );
     expect(wrapper.find(".ant-spin-nested-loading").length).toBe(1);
   });
+
+  it("float should work", () => {
+    const wrapper = mount(<GeneralDrawer visible={true} isFloat={true} />);
+    expect(wrapper.find(".floatDrawer").length).not.toBe(0);
+    expect(wrapper.find(".outerBtn").length).toBe(0);
+
+    wrapper.setProps({ hasOuterSwitch: true });
+    wrapper.update();
+    expect(wrapper.find(".defaultOuterBtn").length).toBe(1);
+
+    wrapper.setProps({
+      useBigOuterSwitch: true,
+      customSwitchConfig: {
+        openIcon: {
+          lib: "easyops",
+          category: "default",
+          icon: "calendar",
+        },
+        closeText: "关闭",
+      },
+    });
+    wrapper.update();
+    expect(wrapper.find(".bigOuterBtn").length).toBe(1);
+  });
 });
