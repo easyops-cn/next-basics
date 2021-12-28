e378901d39297ba15ec771a6141667bb071ba849
weareoutman <wangshenwei@qq.com>
2021-12-28 10:49:34 +0800

    refactor(): refine scanning storyboard

--- bricks/next-builder/src/builder-container/DataView/scanContextsInStoryboard.ts
+++ bricks/next-builder/src/builder-container/DataView/scanContextsInStoryboard.ts
@@ -1,5 +1,10 @@
 import { Storyboard } from "@next-core/brick-types";
-import { isObject, isEvaluable, preevaluate } from "@next-core/brick-utils";
+import {
+  isObject,
+  PrecookHooks,
+  EstreeLiteral,
+  visitStoryboardExpressions,
+} from "@next-core/brick-utils";
 
 const CTX = "CTX";
 
@@ -21,83 +26,64 @@ function scanContextsInAnyByReadOrWrite(data: unknown): {
 } {
   const readContexts = new Set<string>();
   const writeContexts = new Set<string>();
-  collectContexts(data, readContexts, writeContexts);
+  const beforeVisitContext = beforeVisitContextFactory(readContexts);
+  visitStoryboardExpressions(data, beforeVisitContext, CTX);
+  collectWritingContexts(data, writeContexts);
   return {
     readContexts: Array.from(readContexts),
     writeContexts: Array.from(writeContexts),
   };
 }
 
-function collectContexts(
+function beforeVisitContextFactory(
+  readContexts: Set<string>
+): PrecookHooks["beforeVisitGlobal"] {
+  return function beforeVisitContext(node, parent): void {
+    if (node.name === CTX) {
+      const memberParent = parent[parent.length - 1];
+      if (
+        memberParent?.node.type === "MemberExpression" &&
+        memberParent.key === "object"
+      ) {
+        const memberNode = memberParent.node;
+        const property = memberNode.property as unknown as EstreeLiteral;
+        if (!memberNode.computed && memberNode.property.type === "Identifier") {
+          readContexts.add(memberNode.property.name);
+        } else if (
+          memberNode.computed &&
+          property.type === "Literal" &&
+          typeof property.value === "string"
+        ) {
+          readContexts.add(property.value);
+        }
+      }
+    }
+  };
+}
+
+function collectWritingContexts(
   data: unknown,
-  readContexts: Set<string>,
   writeContexts: Set<string>,
   memo = new WeakSet()
 ): void {
-  if (typeof data === "string") {
-    if (data.includes(CTX) && isEvaluable(data)) {
-      try {
-        preevaluate(data, {
-          withParent: true,
-          hooks: {
-            beforeVisitGlobal(node, parent): void {
-              if (node.name === CTX) {
-                const memberParent = parent[parent.length - 1];
-                if (
-                  memberParent?.node.type === "MemberExpression" &&
-                  memberParent.key === "object"
-                ) {
-                  const memberNode = memberParent.node;
-                  if (
-                    !memberNode.computed &&
-                    memberNode.property.type === "Identifier"
-                  ) {
-                    readContexts.add(memberNode.property.name);
-                  } else if (
-                    memberNode.computed &&
-                    (memberNode.property as any).type === "Literal" &&
-                    typeof (memberNode.property as any).value === "string"
-                  ) {
-                    readContexts.add((memberNode.property as any).value);
-                  }
-                }
-              }
-            },
-          },
-        });
-      } catch (error) {
-        // eslint-disable-next-line no-console
-        console.error(
-          "Parse evaluation string failed when scanning contexts:",
-          error
-        );
-      }
-    }
-  } else if (isObject(data)) {
+  if (isObject(data)) {
     // Avoid call stack overflow.
     if (memo.has(data)) {
       return;
     }
     memo.add(data);
-    if (Array.isArray(data)) {
-      for (const item of data) {
-        collectContexts(item, readContexts, writeContexts, memo);
-      }
-    } else {
-      for (const item of Object.values(data)) {
-        collectContexts(item, readContexts, writeContexts, memo);
-      }
-      if (
-        (data.action === "context.replace" ||
-          data.action === "context.assign") &&
-        Array.isArray(data.args) &&
-        typeof data.args[0] === "string" &&
-        // Ignore evaluations and placeholders,
-        // E.g.: `<% QUERY.x %>` or `${QUERY.x}`.
-        !/[<{]/.test(data.args[0])
-      ) {
-        writeContexts.add(data.args[0]);
-      }
+    for (const item of Array.isArray(data) ? data : Object.values(data)) {
+      collectWritingContexts(item, writeContexts, memo);
+    }
+    if (
+      (data.action === "context.replace" || data.action === "context.assign") &&
+      Array.isArray(data.args) &&
+      typeof data.args[0] === "string" &&
+      // Ignore evaluations and placeholders,
+      // E.g.: `<% QUERY.x %>` or `${QUERY.x}`.
+      !/[<{]/.test(data.args[0])
+    ) {
+      writeContexts.add(data.args[0]);
     }
   }
 }
