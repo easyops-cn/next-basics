ced844acdf910149fdf092debab039ae078236af
SailorF <541899641@qq.com>
2022-01-18 19:25:16 +0800

    feat(navBrick): support nav brick
    NEXT_BUILDER-1330

--- bricks/frame-bricks/src/nav-bar/index.tsx
+++ bricks/frame-bricks/src/nav-bar/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrickWrapper, UpdatingElement } from "@next-core/brick-kit";
+import { BrickWrapper, UpdatingElement, property } from "@next-core/brick-kit";
 import { NavBar } from "./NavBar";
+import { SidebarSubMenu } from "@next-core/brick-types";
 
 /**
  * @id frame-bricks.nav-bar
@@ -12,6 +13,27 @@ import { NavBar } from "./NavBar";
  * @noInheritDoc
  */
 export class NavBarElement extends UpdatingElement {
+  /**
+   * @kind SidebarSubMenu
+   * @required false
+   * @default -
+   * @description 菜单项
+   */
+  @property({
+    attribute: false,
+  })
+  menu: SidebarSubMenu;
+
+  /**
+   * @default
+   * @required false
+   * @description 选择菜单
+   */
+  @property({
+    attribute: false,
+  })
+  selectedKeys: string[];
+
   connectedCallback(): void {
     // Don't override user's style settings.
     // istanbul ignore else
@@ -30,7 +52,10 @@ export class NavBarElement extends UpdatingElement {
     if (this.isConnected) {
       ReactDOM.render(
         <BrickWrapper>
-          <NavBar />
+          <NavBar
+            menuItems={this.menu.menuItems}
+            selectedKeys={this.selectedKeys}
+          />
         </BrickWrapper>,
         this
       );
