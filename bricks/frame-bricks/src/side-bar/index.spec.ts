cf8b08faa09e2338a6cbe353a6a431ff6fd09b69
SailorF <541899641@qq.com>
2022-01-25 18:36:45 +0800

    feat(): update unit test
    Refs: NEXT_BUILDER-1330

--- bricks/frame-bricks/src/side-bar/index.spec.ts
+++ bricks/frame-bricks/src/side-bar/index.spec.ts
@@ -17,4 +17,28 @@ describe("frame-bricks.side-bar", () => {
     document.body.removeChild(element);
     expect(unmountComponentAtNode).toBeCalled();
   });
+
+  it(`should dispatch event when mouseEnter has been called"`, async () => {
+    const mouseEnterListener = jest.fn();
+    const element = document.createElement("frame-bricks.side-bar");
+    element.addEventListener("side.bar.enter", mouseEnterListener);
+    document.body.appendChild(element);
+    await jest.runAllTimers();
+    spyOnRender.mock.calls[spyOnRender.mock.calls.length - 1][0][
+      "props"
+    ].children["props"].onMouseEnter();
+    expect(mouseEnterListener).toBeCalled();
+  });
+
+  it(`should dispatch event when mouseLeave has been called"`, async () => {
+    const mouseLeaveListener = jest.fn();
+    const element = document.createElement("frame-bricks.side-bar");
+    element.addEventListener("side.bar.leave", mouseLeaveListener);
+    document.body.appendChild(element);
+    await jest.runAllTimers();
+    spyOnRender.mock.calls[spyOnRender.mock.calls.length - 1][0][
+      "props"
+    ].children["props"].onMouseLeave();
+    expect(mouseLeaveListener).toBeCalled();
+  });
 });
