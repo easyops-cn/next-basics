2b75d276fc1412a451d1e8aa0971402b3f5e6ba8
Wuqimenga <2949683360@qq.com>
2022-01-27 17:16:50 +0800

    fix(): fix the bug that the brick do not tell anything when options and dataSource are not set
    Refs#MONITOR-3017

--- bricks/basic-bricks/src/general-hotkeys/index.spec.ts
+++ bricks/basic-bricks/src/general-hotkeys/index.spec.ts
@@ -8,6 +8,7 @@ const spyOnRender = jest
 describe("basic-bricks.general-hotkeys", () => {
   it("should create a custom element", () => {
     const element = document.createElement("basic-bricks.general-hotkeys");
+    (element as any).hotkeysConfig = [{ key: "f5", eventName: "test" }];
     expect(spyOnRender).not.toBeCalled();
     document.body.appendChild(element);
     expect(spyOnRender).not.toBeCalled();
