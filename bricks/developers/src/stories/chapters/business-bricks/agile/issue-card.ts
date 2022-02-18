fd4100318225806b41c888bab6a396f321af7ed3
MJmajiong <936233505@qq.com>
2022-02-18 21:57:06 +0800

    docs(): issue-card示例

--- bricks/developers/src/stories/chapters/business-bricks/agile/issue-card.ts
+++ bricks/developers/src/stories/chapters/business-bricks/agile/issue-card.ts
@@ -64,6 +64,88 @@ export const story: Story = {
         },
       },
     },
+    {
+      description: {
+        title: "topRightBrick",
+        message: "自定义右上角的内容",
+      },
+      brick: "agile.issue-card",
+      properties: {
+        topRightOperate: {
+          useBrick: {
+            brick: "basic-bricks.general-custom-buttons",
+            events: {
+              "instance.delete": {
+                action: "console.log",
+              },
+              "instance.set.default": {
+                action: "console.log",
+              },
+            },
+            properties: {
+              customButtons: [
+                {
+                  eventName: "instance.set.default",
+                  icon: "setting",
+                  isDropdown: true,
+                  text: "设为默认",
+                },
+                {
+                  color: "#E02020",
+                  eventName: "instance.delete",
+                  icon: "delete",
+                  isDropdown: true,
+                  text: "删除",
+                },
+              ],
+              isMoreButton: true,
+              moreButtonShape: "no",
+            },
+          },
+        },
+        issueInfo: {
+          isShowSubtasksStatistics: true,
+          name: "AGILE_V2-9",
+          id: "59e21fc0610b8",
+          creator: "leon",
+          ctime: "2020-02-09 18:21:22",
+          title: "【编排】作为用户，希望能自由创建issue，以便将各事务统一管理",
+          assignee: [
+            {
+              name: "leon",
+              id: "56ffdd9b599e9",
+              user_icon:
+                "http://p.qlogo.cn/bizmail/d47GDej7HBmeibwD2tQVh8szhfBP6TrZIZcamNxY6xRsm4yHf2C63mA/0",
+            },
+          ],
+          tester: [
+            {
+              name: "alrenhuang",
+              id: "56ffdd9ba8648",
+              user_icon:
+                "http://p.qlogo.cn/bizmail/O1tyN98UWdKvhH2dtsicBEXOOD05XSFBKWe6XjicImtWrLzRFFZBKEqw/0",
+            },
+          ],
+          step: "developed",
+          type: "story",
+          reporter: [
+            {
+              name: "leon",
+              id: "56ffdd9b599e9",
+              user_icon:
+                "http://p.qlogo.cn/bizmail/d47GDej7HBmeibwD2tQVh8szhfBP6TrZIZcamNxY6xRsm4yHf2C63mA/0",
+            },
+          ],
+          priority: "medium",
+          storyPoint: 1.5,
+        },
+        events: {
+          "card.click": {
+            action: "console.log",
+          },
+        },
+      },
+    },
   ],
   doc: docMD,
 };
