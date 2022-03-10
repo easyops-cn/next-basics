216554d20e9374c9f77e0992595150c6d2070eb6
SheRunFeng <541899641@qq.com>
2022-03-10 17:14:29 +0800

    feat(): support merge version and replace theme Refs: NEXT_BUILDER-1433

---
+++ bricks/next-builder/src/data-providers/utils/mergeVersion.ts
@@ -0,0 +1,66 @@
+export interface DependencyItem {
+  name: string;
+  constraint: string;
+  [k: string]: unknown;
+}
+
+export function compareVersion(local: string, remote: string): string | false {
+  const compare = (version1: number[], version2: number[]): number[] => {
+    let index = 0;
+    const length = version1.length;
+    while (index < length) {
+      if (version1[index] === version2[index]) {
+        index++;
+        continue;
+      }
+      if (version1[index] > version2[index]) {
+        return version1;
+      }
+      return version2;
+    }
+    // equal
+    return version1;
+  };
+  if (local.startsWith("^") && remote.startsWith("^")) {
+    //
+    const versionReg = new RegExp(/(\d+).(\d+).(\d+)/);
+    const [_localVersion, ...version1] = local.match(versionReg);
+    const [_remoteVersion, ...version2] = remote.match(versionReg);
+
+    return `^${compare(version1.map(Number), version2.map(Number)).join(".")}`;
+  }
+
+  return false;
+}
+
+export function mergeVersion(
+  curDeps: DependencyItem[],
+  remoteDeps: DependencyItem[]
+): DependencyItem[] {
+  const remoteMap = new Map(remoteDeps.map((item) => [item.name, item]));
+  let result: DependencyItem[] = [];
+  for (let i = 0; i < curDeps.length; i++) {
+    const curDep = curDeps[i];
+    const remoteDep = remoteMap.get(curDep.name);
+    if (remoteDep) {
+      // find, compare the version
+      const version = compareVersion(curDep.constraint, remoteDep.constraint);
+      if (version) {
+        result.push({
+          ...curDep,
+          ...remoteDep,
+          constraint: version as string,
+        });
+      } else {
+        // compare fail, concat both of them
+        result = result.concat([curDep, remoteDep]);
+      }
+      // delete cache
+      remoteMap.delete(curDep.name);
+    } else {
+      result.push(curDep);
+    }
+  }
+
+  return result.concat([...remoteMap.values()]);
+}
