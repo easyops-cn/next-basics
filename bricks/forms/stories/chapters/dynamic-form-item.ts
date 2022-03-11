bbdc8abc58921d8ee1dd05655541d839a1011aba
nlicroshan <nlicroshan@easyops.cn>
2022-03-11 15:02:38 +0800

    chore(forms): deprecate dynamic-form-item

--- bricks/forms/stories/chapters/dynamic-form-item.ts
+++ bricks/forms/stories/chapters/dynamic-form-item.ts
@@ -1,6 +1,7 @@
 import { Story } from "@next-core/brick-types";
 
 export const dynamicFormItemStory: Story = {
+  deprecated: true,
   storyId: "forms.dynamic-form-item",
   type: "brick",
   category: "form-input",
@@ -10,10 +11,8 @@ export const dynamicFormItemStory: Story = {
     zh: "动态表单项",
   },
   description: {
-    en:
-      "Multi-column display, dynamically add or delete each row of form items, currently supports input , select, and other related types",
-    zh:
-      "多列显示可以动态增加或删除的表单项，目前支持 input 和 select 等多种类型",
+    en: "Multi-column display, dynamically add or delete each row of form items, currently supports input , select, and other related types",
+    zh: "多列显示可以动态增加或删除的表单项，目前支持 input 和 select 等多种类型",
   },
   icon: {
     lib: "fa",
