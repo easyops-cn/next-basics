2b72b1a5580ed09f16436072ee3b182a15ed502c
weareoutman <wangshenwei@qq.com>
2022-04-12 09:14:19 +0800

    feat(): add tooltip for workbench-action

--- bricks/next-builder/src/workbench-action/WorkbenchAction.tsx
+++ bricks/next-builder/src/workbench-action/WorkbenchAction.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import classNames from "classnames";
+import { Tooltip } from "antd";
 import type { MenuIcon } from "@next-core/brick-types";
 import { GeneralIcon, Link } from "@next-libs/basic-components";
 
@@ -7,23 +8,27 @@ import styles from "./WorkbenchAction.module.css";
 
 export interface WorkbenchActionProps {
   icon: MenuIcon;
+  tooltip?: string;
   to?: string;
   active?: boolean;
 }
 
 export function WorkbenchAction({
   icon,
+  tooltip,
   to,
   active,
 }: WorkbenchActionProps): React.ReactElement {
   return (
-    <Link
-      className={classNames(styles.action, { [styles.active]: active })}
-      role="button"
-      to={to}
-      noEmptyHref
-    >
-      <GeneralIcon icon={icon} size={24} />
-    </Link>
+    <Tooltip title={tooltip} placement="right">
+      <Link
+        className={classNames(styles.action, { [styles.active]: active })}
+        role="button"
+        to={to}
+        noEmptyHref
+      >
+        <GeneralIcon icon={icon} size={24} />
+      </Link>
+    </Tooltip>
   );
 }
