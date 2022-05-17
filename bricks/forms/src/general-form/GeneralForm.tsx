c7b3476fd79298165ab4aed533e32062ac0222c0
SheRunFeng <541899641@qq.com>
2022-05-17 19:01:26 +0800

    Revert "fix(general-forms): 修改forms默认最大宽度"

--- bricks/forms/src/general-form/GeneralForm.tsx
+++ bricks/forms/src/general-form/GeneralForm.tsx
@@ -74,7 +74,7 @@ export function LegacyGeneralForm({
         className="form-wrap"
         layout={layout}
         style={{
-          ...(maxWidthLimited ? { maxWidth: 800 } : undefined),
+          ...(maxWidthLimited ? { maxWidth: 1332 } : undefined),
           ...formStyle,
         }}
       >
