274ecc235e8820b3c5d517d9540a8bc6885b6966
SheRunFeng <541899641@qq.com>
2022-06-09 19:24:58 +0800

    feat(PreviewContainer): update unit test Refs: NEXT_BUILDER-1619

--- bricks/next-builder/src/preview-container/PreviewContainer.spec.tsx
+++ bricks/next-builder/src/preview-container/PreviewContainer.spec.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { shallow } from "enzyme";
 import { PreviewContainer } from "./PreviewContainer";
+import { useBuilderDataManager } from "@next-core/editor-bricks-helper";
+
+jest.mock("@next-core/editor-bricks-helper");
+(useBuilderDataManager as jest.Mock).mockReturnValue(() => jest.fn());
 
 describe("PreviewContainer", () => {
   it("should work", () => {
