3545300a63a4355a29e35fd4c32fe9e3638c33d1
julielai <julielai@easyops.cn>
2022-06-15 09:57:59 +0800

    fix(): fixDoc close BRICK_STORE-2839 BRICK_STORE-2836 BRICK_STORE-2840

--- bricks/forms/src/general-modal/index.tsx
+++ bricks/forms/src/general-modal/index.tsx
@@ -191,6 +191,7 @@ export class GeneralModalElement extends FormItemElement {
    * @kind `{modalTitle: string}	`
    * @required false
    * @default -
+   * @deprecated
    * @description [已废弃]字段映射, 跟 dataSource 一起使用来获得运行时 modalTitle
    * @group advanced
    */
@@ -353,19 +354,19 @@ export class GeneralModalElement extends FormItemElement {
   /**
    *
    * @description  当点击 modal 自带的取消按钮时发出该事件
-   * @detail `dataSource`
+   * @detail `dataSource: Record<string, any>`
    */
   @event({ type: "basic-bricks.general-modal.cancel" })
   cancelEvent: EventEmitter<Record<string, any>>;
   /**
    *
    * @description  当点击 modal 自带的确认按钮时发出该事件
-   * @detail `dataSource`
+   * @detail `dataSource: Record<string, any>`
    */
   @event({ type: "basic-bricks.general-modal.confirm" })
   confirmEvent: EventEmitter<Record<string, any>>;
   /**
-   * @detail `dataSource`
+   * @detail `dataSource: Record<string, any>`
    * @description  打开 modal 时发出该事件
    */
   @event({ type: "modal.open" }) openEvent: EventEmitter<Record<string, any>>;
@@ -383,7 +384,7 @@ export class GeneralModalElement extends FormItemElement {
     }
   }
   /**
-   * @detail `dataSource`
+   * @detail `dataSource: Record<string, any>`
    * @description  关闭 modal 时发出该事件
    */
   @event({ type: "modal.close" }) closeEvent: EventEmitter<Record<string, any>>;
