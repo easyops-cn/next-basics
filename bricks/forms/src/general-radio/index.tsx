a0e530fea7fb512e9a7c5cf8cb73e1b91f9329a6
kehua <kehuaning@easyops.com>
2022-06-22 17:59:47 +0800

    docs(radio): remove extra @groupI18N

--- bricks/forms/src/general-radio/index.tsx
+++ bricks/forms/src/general-radio/index.tsx
@@ -43,21 +43,6 @@ export interface LabeledValue {
  * 1.207.0: `type` 属性新增  `icon-circle` 类型样式
  * 1.1.0: 构件 type 属性，添加`icon`图标类型样式
  * 1.0.0: 新增构件 `forms.general-radio`
- * @groupI18N
- * {
- *   "basicFormItem": {
- *     "en": "Basic Form Item",
- *     "zh": "表单项常用"
- *   },
- *   "advancedFormItem": {
- *     "en": "Advanced Form Item",
- *     "zh": "表单项高级"
- *   },
- *   "ui": {
- *     "en": "UI",
- *     "zh": "外观"
- *   }
- * }
  * @memo
  * > Tips: 单选框与 general-form 结合使用时，通过 value 设置初始值是无效的，需要在 general-form [values](developers/brick-book/brick/forms.general-form) 属性中设置初始值。
  */
