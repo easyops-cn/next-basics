00ec089e4f19ee9174cb885d3c0624d7dda9e7f8
yingjiechen <1910026791@qq.com>
2022-06-24 17:32:17 +0800

    fix(forms.GeneralAutoComplete): generalAutoComplete文档新增disabled配置

--- bricks/forms/stories/chapters/general-auto-complete.ts
+++ bricks/forms/stories/chapters/general-auto-complete.ts
@@ -17,63 +17,131 @@ export const autoCompleteStory: Story = {
     lib: "fa",
     icon: "pen",
   },
-  conf: {
-    brick: "forms.general-form",
-    properties: {
-      values: {
-        category: "f5",
+  conf: [
+    {
+      brick: "forms.general-form",
+      description: {
+        title: "基本使用",
       },
-    },
-    events: {
-      "validate.success": {
-        action: "console.log",
-        args: ["${EVENT.type}", "${EVENT.detail}"],
+      properties: {
+        values: {
+          category: "f5",
+        },
       },
-      "validate.error": {
-        action: "console.warn",
-        args: ["${EVENT.type}", "${EVENT.detail}"],
+      events: {
+        "validate.success": {
+          action: "console.log",
+          args: ["${EVENT.type}", "${EVENT.detail}"],
+        },
+        "validate.error": {
+          action: "console.warn",
+          args: ["${EVENT.type}", "${EVENT.detail}"],
+        },
       },
-    },
-    slots: {
-      items: {
-        type: "bricks",
-        bricks: [
-          {
-            brick: "forms.general-auto-complete",
-            properties: {
-              dataset: { testid: "basic-usage-demo" },
-              name: "category",
-              label: "二级分类",
-              labelTooltip: "auto complete",
-              required: true,
-              placeholder: "请输入",
-              options: ["网络设备", "存储", "IP管理"],
-            },
-            events: {
-              "general.auto-complete.change": {
-                action: "console.log",
-                args: ["auto-complete", "${EVENT.detail}"],
+      slots: {
+        items: {
+          type: "bricks",
+          bricks: [
+            {
+              brick: "forms.general-auto-complete",
+              properties: {
+                dataset: { testid: "basic-usage-demo" },
+                name: "category",
+                label: "二级分类",
+                labelTooltip: "auto complete",
+                required: true,
+                placeholder: "请输入",
+                options: ["网络设备", "存储", "IP管理"],
+              },
+              events: {
+                "general.auto-complete.change": {
+                  action: "console.log",
+                  args: ["auto-complete", "${EVENT.detail}"],
+                },
               },
             },
-          },
-          {
-            brick: "forms.general-buttons",
-            properties: {
-              showCancelButton: true,
-              submitText: "提交",
-              cancelText: "取消",
-            },
-            events: {
-              "submit.button.click": {
-                action: "console.log",
+            {
+              brick: "forms.general-buttons",
+              properties: {
+                showCancelButton: true,
+                submitText: "提交",
+                cancelText: "取消",
               },
-              "cancel.button.click": {
-                action: "console.log",
+              events: {
+                "submit.button.click": {
+                  action: "console.log",
+                },
+                "cancel.button.click": {
+                  action: "console.log",
+                },
               },
             },
-          },
-        ],
+          ],
+        },
       },
     },
-  },
+    {
+      brick: "forms.general-form",
+      description: {
+        title: "禁用",
+      },
+      properties: {
+        values: {
+          category: "f5",
+        },
+      },
+      events: {
+        "validate.success": {
+          action: "console.log",
+          args: ["${EVENT.type}", "${EVENT.detail}"],
+        },
+        "validate.error": {
+          action: "console.warn",
+          args: ["${EVENT.type}", "${EVENT.detail}"],
+        },
+      },
+      slots: {
+        items: {
+          type: "bricks",
+          bricks: [
+            {
+              brick: "forms.general-auto-complete",
+              properties: {
+                dataset: { testid: "basic-usage-demo" },
+                name: "category",
+                label: "二级分类",
+                labelTooltip: "auto complete",
+                required: true,
+                placeholder: "请输入",
+                options: ["网络设备", "存储", "IP管理"],
+                disabled: true,
+              },
+              events: {
+                "general.auto-complete.change": {
+                  action: "console.log",
+                  args: ["auto-complete", "${EVENT.detail}"],
+                },
+              },
+            },
+            {
+              brick: "forms.general-buttons",
+              properties: {
+                showCancelButton: true,
+                submitText: "提交",
+                cancelText: "取消",
+              },
+              events: {
+                "submit.button.click": {
+                  action: "console.log",
+                },
+                "cancel.button.click": {
+                  action: "console.log",
+                },
+              },
+            },
+          ],
+        },
+      },
+    },
+  ],
 };
