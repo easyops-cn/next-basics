7b4a166fa6616bff28c51a6d9a01dc15102ffbe0
jojiang <jojiang@easyops.cn>
2022-07-06 17:05:19 +0800

    fix(): refine doc
    Refs BRICK_STORE-2945

--- bricks/forms/src/user-or-user-group-select/index.tsx
+++ bricks/forms/src/user-or-user-group-select/index.tsx
@@ -32,26 +32,26 @@ import { FormItemElement } from "@next-libs/forms";
  */
 export class UserOrUserGroupSelectElement extends FormItemElement {
   /**
-   * @kind string
    * @required true
    * @default -
    * @description 下拉框字段名
+   * @group basicFormItem
    */
   @property({ attribute: false }) declare name: string;
 
   /**
-   * @kind string
    * @required false
    * @default -
    * @description 下拉框字段说明
+   * @group basicFormItem
    */
   @property({ attribute: false }) declare label: string;
 
   /**
-   * @kind {selectedUser: string[],selectedUserGroup: string[]}|string[]
    * @required false
    * @default -
    * @description 用户（组）选择构件中下拉框的初始值，按照我们平台的用户（组）数据，selectedUser 为"USER"模型中的 name，selectedUserGroup 为"USER_GROUP"模型中的":"+instanceId。当`mergeUseAndUserGroup`为 true 时，类型为`string[]`。
+   * @group basicFormItem
    */
   @property({
     attribute: false,
@@ -59,26 +59,26 @@ export class UserOrUserGroupSelectElement extends FormItemElement {
   value: string[] | UserOrUserGroupSelectValue;
 
   /**
-   * @kind string
    * @required false
    * @default -
    * @description 下拉框占位说明
+   * @group basicFormItem
    */
   @property({ attribute: false }) declare placeholder: string;
 
   /**
-   * @kind boolean
    * @required false
    * @default -
    * @description 是否必填项
+   * @group basicFormItem
    */
   @property({ type: Boolean }) declare required: boolean;
 
   /**
-   * @kind boolean
    * @required false
    * @default false
    * @description 是否隐藏“快速选择我”按钮
+   * @group ui
    */
   @property({
     type: Boolean,
@@ -86,10 +86,10 @@ export class UserOrUserGroupSelectElement extends FormItemElement {
   hideAddMeQuickly: boolean;
 
   /**
-   * @kind string[]
    * @required false
    * @default -
    * @description 固定白名单列表，该列表中的值用户不能取消。
+   * @group basicFormItem
    */
   @property({
     attribute: false,
@@ -97,10 +97,10 @@ export class UserOrUserGroupSelectElement extends FormItemElement {
   staticList: string[];
 
   /**
-   * @kind boolean
    * @required false
    * @default false
    * @description 是否合并用户和用户组数据，当设置为 true 时，输入的`value`和`user.group.change`事件输出的 detail 都为`string[]`格式。
+   * @group basicFormItem
    */
   @property({
     type: Boolean,
@@ -108,30 +108,28 @@ export class UserOrUserGroupSelectElement extends FormItemElement {
   mergeUseAndUserGroup: boolean;
 
   /**
-   * @kind "all"|"group"|"user"
    * @required false
-   * @default "all"
+   * @default all
    * @description 支持选择用户、用户组或者两者
+   * @group ui
    */
   @property({ attribute: false })
   optionsMode: "user" | "group" | "all" = "all";
 
   /**
-   * @kind boolean
    * @required false
    * @default false
    * @description 隐藏无效用户
-   * @group advanced
+   * @group ui
    */
   @property({ type: Boolean })
   hideInvalidUser: boolean;
 
   /**
-   * @kind boolean
    * @required false
    * @default false
    * @description 是否隐藏搜索 icon，即不支持通过 cmdb 的 modal 选择器选择
-   * @group advanced
+   * @group ui
    */
   @property({
     type: Boolean,
@@ -139,11 +137,10 @@ export class UserOrUserGroupSelectElement extends FormItemElement {
   hideSelectByCMDB: boolean;
 
   /**
-   * @kind `Record<string, any>`
    * @required false
    * @default -
    * @description  用户和用户组`search`接口的`query`，此参数比较适用于，两者接口需要参数相同的情况下使用
-   * @group advanced
+   * @group basicFormItem
    */
   @property({
     attribute: false,
@@ -151,21 +148,19 @@ export class UserOrUserGroupSelectElement extends FormItemElement {
   query: Record<string, any>;
 
   /**
-   * @kind CmdbModels.ModelCmdbObject
    * @required true
    * @default -
    * @description 模型列表，直接来自"providers-of-cmdb.cmdb-object-api-get-object-all"
-   * @group advanced
+   * @group basicFormItem
    */
   @property({ attribute: false })
   objectList: Partial<CmdbModels.ModelCmdbObject>[];
 
   /**
-   * @kind `Record<string, any>`
    * @required false
    * @default -
    * @description 针对`USER/instance/_search`接口的`query`，此参数比较适用于，可能只需要针对用户做筛选的情况
-   * @group advanced
+   * @group basicFormItem
    */
   @property({
     attribute: false,
@@ -173,11 +168,10 @@ export class UserOrUserGroupSelectElement extends FormItemElement {
   userQuery: Record<string, any>;
 
   /**
-   * @kind `Record<string, any>`
    * @required false
    * @default -
    * @description 针对`USER_GROUP/instance/_search`接口的`query`，此参数比较适用于，可能只需要针对用户组做筛选的情况
-   * @group advanced
+   * @group basicFormItem
    */
   @property({
     attribute: false,
