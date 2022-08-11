6125cb2fda54911b293d926b14b622ea5c8e1b5e
SheRunFeng <541899641@qq.com>
2022-08-11 21:54:52 +0800

    chore(): delete unuse code

--- bricks/next-previewer/src/preview-helper/previewStart.ts
+++ bricks/next-previewer/src/preview-helper/previewStart.ts
@@ -97,17 +97,6 @@ export function previewStart(
     }
   };
 
-  const handleMatchCache = (num: number): void => {
-    window.parent.postMessage(
-      {
-        sender: "previewer",
-        type: "match-api-cache",
-        num,
-      },
-      previewProxyOrigin
-    );
-  };
-
   const sendHighlightBrickOutlines = (
     type: "hover" | "active",
     iid: string,
@@ -194,9 +183,6 @@ export function previewStart(
             break;
           case "hover-on-iframe":
             handleHoverOnIframe(data.position);
-            break;
-          case "match-api-cache":
-            handleMatchCache(data.num);
             break;
         }
       } else
