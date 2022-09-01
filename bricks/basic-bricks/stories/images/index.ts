b6c13b072aabad42bbb2aea99b499f80dbc6d0c5
jojiang <jojiang@easyops.cn>
2022-09-01 15:04:02 +0800

    replace new icon
    Refs BRICK_STORE-3050

---
+++ bricks/basic-bricks/stories/images/index.ts
@@ -0,0 +1,5 @@
+/// <reference types="../../../../declarations/global" />
+
+export { default as easyViewSvg } from "./basic-bricks.easy-view.svg";
+export { default as microViewSvg } from "./basic-bricks.micro-view.svg";
+export { default as generalButtonSvg } from "./basic-bricks.general-button.svg";
