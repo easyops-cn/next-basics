3d3b0e5980a5596a4719ccab06fa146d0626c72b
MJmajiong <936233505@qq.com>
2022-09-09 15:46:45 +0800

    docs(): 修复tree-select.svg引入 refs BRICK_STORE-3060

--- bricks/forms/stories/images/index.ts
+++ bricks/forms/stories/images/index.ts
@@ -14,4 +14,4 @@ export { default as inputWithUnit } from "./forms.input-with-unit.svg";
 export { default as timeRangePicker } from "./forms.time-range-picker.svg";
 export { default as uploadFilesV2 } from "./forms.upload-files-v2.svg";
 export { default as uploadFiles } from "./forms.upload-files.svg";
-export { default as treeSelect } from "../forms.tree-select.svg";
+export { default as treeSelect } from "./forms.tree-select.svg";
