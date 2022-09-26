457e41eb2719efb92a1d2df246247d2561c4bb61
kehua <kehuaning@easyops.com>
2022-09-26 16:01:47 +0800

    fix(): 完善logobar的样式

--- bricks/basic-bricks/src/menu-bar/LogoBar/LogoBar.tsx
+++ bricks/basic-bricks/src/menu-bar/LogoBar/LogoBar.tsx
@@ -14,7 +14,12 @@ export function LogoBar(): React.ReactElement {
   const logo: React.ReactElement = brand.menu_bar_logo_url ? (
     <img
       src={brand.menu_bar_logo_url}
-      style={{ height: isUseUI8 ? 24 : 32, verticalAlign: "top" }}
+      style={{
+        height: isUseUI8 ? 24 : 32,
+        verticalAlign: "top",
+        paddingRight: isUseUI8 ? 12 : 0,
+        cursor: "pointer",
+      }}
     />
   ) : isUseUI8 ? (
     <GeneralIcon
