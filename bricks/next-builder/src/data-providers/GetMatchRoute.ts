725169a814d9677e28e2632e76c077991ce15308
SheRunFeng <541899641@qq.com>
2022-10-10 11:56:57 +0800

    feat(): get match-route provider

---
+++ bricks/next-builder/src/data-providers/GetMatchRoute.ts
@@ -0,0 +1,26 @@
+import { RouteConf } from "@next-core/brick-types";
+import { createProviderClass, matchPath } from "@next-core/brick-utils";
+
+export interface GetMatchRouteParams {
+  routesList: RouteConf[];
+  previewUrl: string;
+}
+
+export function GetMatchRoute({
+  routesList,
+  previewUrl,
+}: GetMatchRouteParams): RouteConf {
+  return routesList
+    .sort((a, b) => Number(b.exact) - Number(a.exact))
+    .find((route) => {
+      return matchPath(previewUrl, {
+        path: route.path,
+        exact: route.exact,
+      });
+    });
+}
+
+customElements.define(
+  "next-builder.provider-get-match-route",
+  createProviderClass(GetMatchRoute)
+);
