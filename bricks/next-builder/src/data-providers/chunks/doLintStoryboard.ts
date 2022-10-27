983ab6c96bff957cccbaccf2670044c70184fb40
weareoutman <wangshenwei@qq.com>
2022-10-27 16:31:44 +0800

    fix(): refine storyboard linting
    Warn if setting tag name as target

--- bricks/next-builder/src/data-providers/chunks/doLintStoryboard.ts
+++ bricks/next-builder/src/data-providers/chunks/doLintStoryboard.ts
@@ -12,7 +12,7 @@ import {
 export type StoryboardErrorCode = "SCRIPT_BRICK" | "TAG_NAME_AS_TARGET";
 
 export interface StoryboardError {
-  type?: "warn" | "error";
+  type: "warn" | "error";
   code: StoryboardErrorCode;
   message: I18nData;
 }
@@ -24,6 +24,7 @@ export function doLintStoryboard(storyboard: Storyboard): StoryboardError[] {
   const { bricks } = scanStoryboardAst(ast);
   if (bricks.includes("basic-bricks.script-brick")) {
     errors.push({
+      type: "error",
       code: "SCRIPT_BRICK",
       message: {
         zh: "您正在使用 `basic-bricks.script-brick`，而它被认为不可维护、兼容性差。请使用微应用函数或自定义处理函数来代替。",
@@ -40,6 +41,7 @@ export function doLintStoryboard(storyboard: Storyboard): StoryboardError[] {
         if (typeof target === "string") {
           if (invalidTargetRegExp.test(target)) {
             errors.push({
+              type: "error",
               code: "TAG_NAME_AS_TARGET",
               message: {
                 zh: `您正在使用构件名作为事件目标: "${target}"，这可能导致意外选中其他构件，请改为使用 id 或 ref。`,
