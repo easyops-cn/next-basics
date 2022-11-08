66403b752c865f6995c547418ef5c4388fb154f9
李佳明 <jimmyli@easyops.cn>
2022-11-08 12:15:53 +0800

    fix(): 修复standalone模式下, home-redirect不生效的问题
    refs NEXT_BUILDER-2193

--- bricks/basic-bricks/src/home-redirect/index.spec.ts
+++ bricks/basic-bricks/src/home-redirect/index.spec.ts
@@ -5,17 +5,19 @@ jest.spyOn(kit, "getRuntime").mockReturnValue({
   getMicroApps: () => [
     {
       id: "search",
-      homepage: "/search"
+      homepage: "/search",
     },
     {
-      id: "no-homepage"
-    }
-  ]
+      id: "no-homepage",
+    },
+  ],
 } as any);
 const spyOnReplace = jest.fn();
 jest.spyOn(kit, "getHistory").mockReturnValue({
-  replace: spyOnReplace
+  replace: spyOnReplace,
 } as any);
+
+const location = window.location;
 
 describe("basic-bricks.home-redirect", () => {
   let element: any;
@@ -26,6 +28,9 @@ describe("basic-bricks.home-redirect", () => {
     await jest.runAllTimers();
     document.body.appendChild(element);
     await jest.runAllTimers();
+    window.STANDALONE_MICRO_APPS = undefined;
+    delete window.location;
+    window.location = location;
   });
 
   afterEach(() => {
@@ -45,9 +50,53 @@ describe("basic-bricks.home-redirect", () => {
     expect(spyOnReplace).not.toBeCalled();
   });
 
-  it("should do nothing if app not found", async () => {
+  it("should do nothing if app not found and not rediectUrl", async () => {
     element.appId = "not-found";
     await jest.runAllTimers();
     expect(spyOnReplace).not.toBeCalled();
+  });
+
+  it("should redicect to specific url if app not found", async () => {
+    element.appId = "not-found";
+    element.redirectUrl = "/some-homepage";
+    await jest.runAllTimers();
+    expect(spyOnReplace).toBeCalledWith("/some-homepage");
+  });
+
+  it("should redicect to specific url if app not found, standalone mode", async () => {
+    window.STANDALONE_MICRO_APPS = true;
+    delete window.location;
+    window.location = {
+      origin: location.origin,
+      replace: jest.fn(),
+      reload: jest.fn(),
+      assign: jest.fn(),
+    } as unknown as Location;
+
+    element.appId = "not-found";
+    element.redirectUrl = "/some-homepage";
+    await jest.runAllTimers();
+    expect(window.location.replace).toBeCalledWith("some-homepage");
+  });
+
+  it("should redicect to specific url", async () => {
+    element.redirectUrl = "/some-homepage2";
+    await jest.runAllTimers();
+    expect(spyOnReplace).toBeCalledWith("/some-homepage2");
+  });
+
+  it("should redicect to specific url, standalone mode", async () => {
+    window.STANDALONE_MICRO_APPS = true;
+    delete window.location;
+    window.location = {
+      origin: location.origin,
+      replace: jest.fn(),
+      reload: jest.fn(),
+      assign: jest.fn(),
+    } as unknown as Location;
+
+    element.redirectUrl = "/some-homepage3/haha";
+    await jest.runAllTimers();
+    expect(window.location.replace).toBeCalledWith("some-homepage3/haha");
   });
 });
--- bricks/basic-bricks/src/home-redirect/index.ts
+++ bricks/basic-bricks/src/home-redirect/index.ts
@@ -29,23 +29,39 @@ import {
 export class HomeRedirectElement extends UpdatingElement {
   /**
    * @kind string
-   * @required true
+   * @required false
    * @default -
    * @description 指定的应用 Id
    */
   @property()
   appId: string;
 
+  /**
+   * @kind string
+   * @required false
+   * @default -
+   * @description 指定的跳转 url
+   */
+  @property()
+  redirectUrl: string;
+
   connectedCallback(): void {
     this._render();
   }
 
   protected _render(): void {
-    if (this.isConnected && this.appId) {
+    if (this.isConnected && (this.appId || this.redirectUrl)) {
       const apps = getRuntime().getMicroApps({ excludeInstalling: true });
       const app = apps.find((item) => item.id === this.appId);
       if (app && app.homepage) {
         getHistory().replace(app.homepage);
+      } else if (this.redirectUrl) {
+        // 找不到app，则fallback到指定跳转url
+        if (window.STANDALONE_MICRO_APPS) {
+          window.location.replace(this.redirectUrl.replace(/^\/*/, ""));
+        } else {
+          getHistory().replace(this.redirectUrl);
+        }
       }
     }
   }
