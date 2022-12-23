f1378b6d566ccaa048ba6f53b56abd0695f08ca9
frankshi <8533334@qq.com>
2022-12-23 11:39:54 +0800

    feat(): business rule suport list Refs:FORM_BUILDER-179

--- bricks/form-builder/src/business-rule/BusinessRule.tsx
+++ bricks/form-builder/src/business-rule/BusinessRule.tsx
@@ -1,40 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./BusinessRule.module.css";
 import {
   EllipsisOutlined,
   EditOutlined,
   DeleteOutlined,
 } from "@ant-design/icons";
-import { Popover } from "antd";
+import { Popover, Empty } from "antd";
 
 export interface TextShow {
   show: string;
   text: string;
 }
 
-interface BusinessRuleProps {
-  textArray: [TextShow[]];
+export interface DataSource {
   ruleTitle: string;
-  dataSource?: any;
+  textArray: TextShow[][];
+}
+
+interface BusinessRuleProps {
+  dataSource?: DataSource[];
+  eventDataSource?: any;
   handleEdit: (dataSource: any) => void;
   handleDelete: (dataSource: any) => void;
 }
 
 export function BusinessRule(props: BusinessRuleProps): React.ReactElement {
+  const { dataSource, eventDataSource, handleEdit, handleDelete } = props;
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    const fn = () => {
+      setVisible(false);
+    };
+    document.addEventListener("click", fn, true);
+    return () => {
+      document.removeEventListener("click", fn, true);
+    };
+  }, []);
+
   const toolContent = (
     <>
       <div
         className={styles.toolItem}
-        onClick={() => props.handleEdit && props.handleEdit(props.dataSource)}
+        onClick={() => {
+          setVisible(false);
+          handleEdit && handleEdit(eventDataSource);
+        }}
       >
         <EditOutlined />
         <span>编辑</span>
       </div>
       <div
         className={`${styles.toolItem} ${styles.danger}`}
-        onClick={() =>
-          props.handleDelete && props.handleDelete(props.dataSource)
-        }
+        onClick={() => {
+          setVisible(false);
+          handleDelete && handleDelete(eventDataSource);
+        }}
       >
         <DeleteOutlined />
         <span>删除</span>
@@ -42,8 +63,8 @@ export function BusinessRule(props: BusinessRuleProps): React.ReactElement {
     </>
   );
 
-  const getText = () => {
-    return props.textArray?.map((item: TextShow[], index: number) => {
+  const getText = (textArray: TextShow[][]) => {
+    return textArray?.map((item: TextShow[], index: number) => {
       return (
         <div key={index}>
           {item?.map((val: TextShow) => {
@@ -58,19 +79,28 @@ export function BusinessRule(props: BusinessRuleProps): React.ReactElement {
     });
   };
 
+  const getCard = () => {
+    return dataSource?.map((item, index) => {
+      return (
+        <div key={index} className={styles.cardWrap}>
+          <div className={styles.cardHead}>
+            <h4>{item.ruleTitle}</h4>
+            <div className={styles.cardTool}>
+              <Popover content={toolContent} visible={visible} trigger="click">
+                <EllipsisOutlined onClick={() => setVisible(!visible)} />
+              </Popover>
+            </div>
+          </div>
+          <div className={styles.cardBody}>{getText(item.textArray)}</div>
+        </div>
+      );
+    });
+  };
+
   return (
     <div data-testid="my-brick">
-      <div className={styles.cardWrap}>
-        <div className={styles.cardHead}>
-          <h4>{props.ruleTitle}</h4>
-          <div className={styles.cardTool}>
-            <Popover content={toolContent} trigger="click">
-              <EllipsisOutlined />
-            </Popover>
-          </div>
-        </div>
-        <div className={styles.cardBody}>{getText()}</div>
-      </div>
+      {dataSource && dataSource.length > 0 && getCard()}
+      {(!dataSource || (dataSource && dataSource.length === 0)) && <Empty />}
     </div>
   );
 }
