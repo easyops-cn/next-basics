8ff598d66f45c6c16c1c16b0aeabbac6535af724
frankshi <8533334@qq.com>
2022-12-28 18:54:05 +0800

    fix(): 规则列表兼容各种数据呈现 Refs:FORM_BUILDER-179

--- bricks/form-builder/src/business-rule/BusinessRule.tsx
+++ bricks/form-builder/src/business-rule/BusinessRule.tsx
@@ -13,12 +13,51 @@ export interface TextShow {
   text: string;
 }
 
+interface Action {
+  actionType: string;
+  target: string;
+}
+
 export interface DataSource {
-  title: string;
-  textArray: TextShow[][];
+  title?: string;
   visible?: boolean;
   uid?: string;
 }
+
+const actionTypeMap = {
+  show: "显示",
+  hide: "隐藏",
+};
+
+interface Condition {
+  origin: string;
+  operation: string;
+  value: string;
+  op: string;
+  conditionId?: string;
+}
+
+interface Group {
+  groupId: string;
+  conditions: Condition[];
+}
+
+interface GroupCondition {
+  groups: Group[];
+  op?: string;
+}
+
+const opMap = {
+  and: "并且",
+  or: "或",
+};
+
+const operationMap = {
+  equal: "等于",
+  notEqual: "不等于",
+  contain: "包含",
+  notContain: "不包含",
+};
 
 interface BusinessRuleProps {
   dataSource?: DataSource[];
@@ -29,7 +68,6 @@ interface BusinessRuleProps {
 export function BusinessRule(props: BusinessRuleProps): React.ReactElement {
   const { handleEdit, handleDelete } = props;
   const [dataSource, setDataSource] = useState([]);
-
   useEffect(() => {
     if (!props.dataSource) {
       return;
@@ -52,20 +90,104 @@ export function BusinessRule(props: BusinessRuleProps): React.ReactElement {
     };
   }, [props.dataSource]);
 
-  const getText = (textArray: TextShow[][]) => {
-    return textArray?.map((item: TextShow[], index: number) => {
+  const getActionsText = (actions: Action[]) => {
+    return actions.map((item) => {
       return (
-        <div key={index}>
-          {item?.map((val: TextShow) => {
-            return (
-              <span key={val.text} className={styles[val.show]}>
-                {val.text}
-              </span>
-            );
-          })}
+        <div key={item.target}>
+          <span>{actionTypeMap[item.actionType]}：</span>
+          <span className={styles.highlight}>{item.target}</span>
         </div>
       );
     });
+  };
+
+  const getConditionsText = (conditions: GroupCondition) => {
+    const op = conditions.op;
+    const groups = conditions?.groups ?? [];
+    return (
+      groups.length > 0 && (
+        <div>
+          <span className={styles.normal}>当满足条件：</span>
+          {groups.map((item, index) => {
+            return (
+              <>
+                <span>
+                  (
+                  {item.conditions.map((v, i) => {
+                    return (
+                      <>
+                        <span className={styles.highlight} key={v.origin}>
+                          {v.origin} {operationMap[v.operation]} {v.value}
+                        </span>
+                        {item.conditions.length - 1 !== i && (
+                          <span>{opMap[v.op]}</span>
+                        )}
+                      </>
+                    );
+                  })}
+                  )
+                </span>
+                {groups.length - 1 !== index && <span>{opMap[op]}</span>}
+              </>
+            );
+          })}
+        </div>
+      )
+    );
+  };
+
+  const getText = (item: any) => {
+    if (item.actionType === "display") {
+      return (
+        <>
+          {getConditionsText(item.conditions)}
+          {getActionsText(item.actions)}
+        </>
+      );
+    } else if (item.actionType === "linkage") {
+      return (
+        <>
+          {getConditionsText(item.conditions)}
+          <div>
+            <span className={styles.highlight}>{item.origin}</span>{" "}
+            数据变更时触发{" "}
+            <span className={styles.highlight}>{item.target}</span> 脚本执行
+          </div>
+        </>
+      );
+    } else if (item.actionType === "submit") {
+      return (
+        <>
+          <span>
+            表单提交时触发{" "}
+            <span className={styles.highlight}>{item.target}</span> 脚本执行
+          </span>
+        </>
+      );
+    } else if (item.actionType === "update") {
+      return (
+        <>
+          {getConditionsText(item.conditions)}
+          <div>
+            <span className={styles.highlight}>{item.origin}</span>{" "}
+            数据变更时触发{" "}
+            <span className={styles.highlight}>{item.target}</span> 脚本执行
+          </div>
+        </>
+      );
+    } else if (item.actionType === "setting") {
+      return (
+        <>
+          {getConditionsText(item.conditions)}
+          <div>
+            <span className={styles.highlight}>{item.origin}</span>{" "}
+            数据变更时设置{" "}
+            <span className={styles.highlight}>{item.target}</span> 值为{" "}
+            {item.settingValue}
+          </div>
+        </>
+      );
+    }
   };
 
   const getCard = () => {
@@ -114,7 +236,7 @@ export function BusinessRule(props: BusinessRuleProps): React.ReactElement {
               </Popover>
             </div>
           </div>
-          <div className={styles.cardBody}>{getText(item.textArray)}</div>
+          <div className={styles.cardBody}>{getText(item)}</div>
         </div>
       );
     });
