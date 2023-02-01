5c932463ebb5372a4fffd81a929aecf21868e055
李佳明 <jimmyli@easyops.cn>
2023-02-01 17:58:25 +0800

    fix(): 解决launchpad没有使用cdn的问题
    refs NEXT_BUILDER-2568

--- bricks/basic-bricks/src/app-bar/utils/getImageUrl.tsx
+++ bricks/basic-bricks/src/app-bar/utils/getImageUrl.tsx
@@ -6,11 +6,15 @@ export function getImageUrl(app: MicroApp, defaultAppIcon: string): string {
       ? app.icons.large
       : `${
           window.STANDALONE_MICRO_APPS
-            ? // 如果是独立打包模式下打开的图片
+            ? // 如果是独立打包模式
               app.standaloneMode
-              ? // 如果微应用是独立应用, 需要带独立打包默认前缀
-                `/sa-static/${app.id}/versions/${app.currentVersion}/webroot/-/micro-apps/${app.id}/${app.icons.large}`
-              : // 否则使用PUBLIC_CDN + 默认前缀
+              ? // 如果是独立打包模式下的standalone app, 则使用 PUBLIC_ROOT 再去掉末尾的 '-/'
+                // 例如: sa-static/-/my-app/versions/1.1.1/webroot/-/micro-apps/my-app/large.png （没设置cdn）
+                // 例如: https://cdn-air.uwintech.cn/next/sa-static/-/my-app/versions/1.1.1/webroot/-/micro-apps/my-app/large.png （设置了cdn）
+                `${window.PUBLIC_ROOT.replace(/-\/$/, "")}${app.id}/versions/${
+                  app.currentVersion
+                }/webroot/-/micro-apps/${app.id}/${app.icons.large}`
+              : // 如果是独立打包模式下的普通app，则使用PUBLIC_CDN + 默认前缀micro-apps
                 `${window.PUBLIC_CDN || ""}micro-apps/${app.id}/${
                   app.icons.large
                 }`
