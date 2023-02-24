b9584af7a3ceb4ea4023403864f6dd5ec0e8131c
kehua <kehuaning@easyops.com>
2023-02-24 11:23:26 +0800

    feat(): feat visual-form-rules-setting
    Ref: NEXT_BUILDER-2641

---
+++ bricks/brick-visualization/src/visual-form-rules-setting/components/ListEditor/ListEditor.tsx
@@ -0,0 +1,256 @@
+import React, { useState, useEffect, useCallback, useMemo } from "react";
+import { GeneralIcon } from "@next-libs/basic-components";
+import { Form, Popover } from "antd";
+import styles from "./ListEditor.module.css";
+
+const COLUMN_KEY = Symbol.for("column_key");
+
+interface ListEditor {
+  name?: string;
+  label?: string | React.ReactElement;
+  value?: any[];
+  getDefaultItem: (data: any) => Record<string, any>;
+  renderFormItem?: (data: any) => React.ReactElement;
+  onChange?: (data: any) => void;
+}
+
+interface Action {
+  actionType: string;
+  target: string;
+}
+
+const actionTypeMap = {
+  show: "显示",
+  hide: "隐藏",
+};
+
+interface Condition {
+  origin: string;
+  operation: string;
+  value: string;
+  op: string;
+  conditionId?: string;
+}
+
+interface Group {
+  groupId: string;
+  conditions: Condition[];
+}
+
+interface GroupCondition {
+  groups: Group[];
+  op?: string;
+}
+
+const opMap = {
+  and: "并且",
+  or: "或",
+};
+
+const operationMap = {
+  equal: "等于",
+  notEqual: "不等于",
+  contain: "包含",
+  notContain: "不包含",
+};
+
+let key = 0;
+
+export function ListEditor({
+  value,
+  getDefaultItem,
+  renderFormItem,
+  onChange,
+}: ListEditor): React.ReactElement {
+  const [list, setList] = useState<any[]>(
+    (value ?? []).map((item) => ({
+      ...item,
+      [COLUMN_KEY]: ++key,
+    }))
+  );
+
+  useEffect(() => {
+    setList(
+      (value ?? []).map((item) => ({
+        ...item,
+        [COLUMN_KEY]: ++key,
+      }))
+    );
+  }, [value]);
+
+  const getActionsText = (actions: Action[]) => {
+    return actions.map((item) => {
+      return (
+        <span key={item.target}>
+          <span>{actionTypeMap[item.actionType]}</span>(
+          <span className={styles.highlight}>{item.target}</span>)&nbsp;
+        </span>
+      );
+    });
+  };
+
+  const getShowText = (item: any) => {
+    const conditions: GroupCondition = item.conditions;
+    const op = conditions.op;
+    const groups = conditions?.groups ?? [];
+    return (
+      groups.length > 0 && (
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <span className={styles.ruleTitle}>{item.title} </span>
+          <span style={{ display: "flex", flexDirection: "column" }}>
+            <span>
+              <span className={styles.normal}>当满足条件</span>
+              {groups.map((item, index) => {
+                return (
+                  <>
+                    <span>
+                      (
+                      {item.conditions.map((v, i) => {
+                        return (
+                          <>
+                            <span className={styles.highlight} key={v.origin}>
+                              {v.origin} {operationMap[v.operation]} {v.value}
+                            </span>
+                            {item.conditions.length - 1 !== i && (
+                              <span>{opMap[v.op]}</span>
+                            )}
+                          </>
+                        );
+                      })}
+                      )
+                    </span>
+                    {groups.length - 1 !== index && <span>{opMap[op]}</span>}
+                  </>
+                );
+              })}
+            </span>
+            <span>{getActionsText(item.actions)}</span>
+          </span>
+        </div>
+      )
+    );
+  };
+
+  const handleRemoveItem = useCallback(
+    (e: React.MouseEvent, item: any): void => {
+      e.stopPropagation();
+      const key = item[COLUMN_KEY];
+      const newList = list.filter((item) => item[COLUMN_KEY] !== key);
+      setList(newList);
+      onChange(newList);
+    },
+    [list, onChange]
+  );
+
+  const handleAddListItem = (): void => {
+    key++;
+    const defaultValue = `newItem(${list.length})`;
+    const newList = list.concat([
+      { ...getDefaultItem(defaultValue), [COLUMN_KEY]: key },
+    ]);
+    setList(newList);
+    onChange?.(newList);
+  };
+
+  const renderListForm = useCallback(
+    (item: any): React.ReactElement => {
+      const handleOnValuesChange = (changeValues: any): void => {
+        const key = item[COLUMN_KEY];
+        const newList = list.map((item) => {
+          if (item[COLUMN_KEY] === key) {
+            item = {
+              ...item,
+              ...changeValues,
+            };
+          }
+          return item;
+        });
+        setList(newList);
+        onChange?.(newList);
+      };
+      return (
+        <Form
+          layout="horizontal"
+          labelAlign="left"
+          labelCol={{
+            style: {
+              minWidth: "112px",
+            },
+          }}
+          onValuesChange={handleOnValuesChange}
+          initialValues={{ ...item }}
+        >
+          {renderFormItem(item)}
+        </Form>
+      );
+    },
+    [list, onChange, renderFormItem]
+  );
+
+  const renderListContent = useMemo((): React.ReactElement => {
+    return (
+      <div className={styles.listContent}>
+        {list.length ? (
+          list.map((item, index) => {
+            return (
+              <Popover
+                key={index}
+                overlayStyle={{
+                  zIndex: 999,
+                }}
+                content={renderListForm(item)}
+                title="Detail"
+                trigger="click"
+                placement="left"
+              >
+                <div className={styles.listItem}>
+                  <span>{getShowText(item)}</span>
+                  <span
+                    onClick={(e) => handleRemoveItem(e, item)}
+                    style={{ display: "flex", alignItems: "center" }}
+                  >
+                    <GeneralIcon
+                      icon={{
+                        lib: "easyops",
+                        category: "assets-inventory",
+                        icon: "out",
+                        color: "red",
+                      }}
+                      style={{
+                        padding: "2px",
+                        border: "1px solid",
+                        borderRadius: "50%",
+                      }}
+                    />
+                  </span>
+                </div>
+              </Popover>
+            );
+          })
+        ) : (
+          <div className={styles.listEmptyItem}>No data</div>
+        )}
+      </div>
+    );
+  }, [list, renderListForm, handleRemoveItem]);
+
+  return (
+    <div className={styles.listContainer}>
+      <div className={styles.listContent}>{renderListContent}</div>
+      <span className={styles.addListItem} onClick={handleAddListItem}>
+        <GeneralIcon
+          icon={{
+            lib: "easyops",
+            category: "assets-inventory",
+            icon: "xin",
+            color: "blue",
+          }}
+          style={{
+            marginRight: "4px",
+          }}
+        />
+        添加
+      </span>
+    </div>
+  );
+}
---
+++ bricks/brick-visualization/src/visual-form-rules-setting/index.tsx
@@ -0,0 +1,96 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrickWrapper, property } from "@next-core/brick-kit";
+import { VisualFormRulesSetting } from "./VisualFormRulesSetting";
+import { FormItemElement } from "@next-libs/forms";
+/**
+ * @id brick-visualization.visual-form-rules-setting
+ * @author kehua
+ * @history
+ * 1.x.0: 新增构件 `brick-visualization.visual-form-rules-setting`
+ * @docKind brick
+ * @noInheritDoc
+ */
+export class VisualFormRulesSettingElement extends FormItemElement {
+  /**
+   * @kind Record<string, any>[]
+   * @default
+   * @required -
+   * @description 初始值
+   */
+  @property({ attribute: false })
+  value: Record<string, any>[];
+
+  /**
+   * @kind Record<string, any>[]
+   * @default
+   * @required -
+   * @description 表单子项数据
+   */
+  @property({ attribute: false })
+  formChildren: Record<string, any>[];
+
+  /**
+   * @kind string
+   * @required false
+   * @default -
+   * @description 表单项字段名
+   * @group basicFormItem
+   */
+  @property({ attribute: false }) declare name: string;
+
+  /**
+   * @kind string
+   * @required false
+   * @default -
+   * @description 表单项字段说明
+   * @group basic
+   */
+  @property({ attribute: false }) declare label: string;
+
+  /**
+   * @kind string
+   * @required false
+   * @default -
+   * @description 表单项字段说明
+   * @group basic
+   */
+  @property({ attribute: false }) declare labelTooltip: string;
+
+  disconnectedCallback(): void {
+    ReactDOM.unmountComponentAtNode(this);
+  }
+
+  connectedCallback(): void {
+    // Don't override user's style settings.
+    // istanbul ignore else
+    if (!this.style.display) {
+      this.style.display = "block";
+    }
+    this._render();
+  }
+
+  protected _render(): void {
+    // istanbul ignore else
+    if (this.isConnected) {
+      ReactDOM.render(
+        <BrickWrapper>
+          <VisualFormRulesSetting
+            formElement={this.getFormElement()}
+            value={this.value}
+            label={this.label}
+            name={this.name}
+            labelTooltip={this.labelTooltip}
+            formChildren={this.formChildren}
+          />
+        </BrickWrapper>,
+        this
+      );
+    }
+  }
+}
+
+customElements.define(
+  "brick-visualization.visual-form-rules-setting",
+  VisualFormRulesSettingElement
+);
