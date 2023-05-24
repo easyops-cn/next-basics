05f842ef6445d80f8ef812c5c90e977832d6a589
nlicroshan <nlicroshan@easyops.cn>
2023-05-24 17:11:07 +0800

    feat(): 新三级菜单样式
    Closes DESIGNER_ROUTINE-649

--- bricks/frame-bricks/src/nav-menu/NavMenu.tsx
+++ bricks/frame-bricks/src/nav-menu/NavMenu.tsx
@@ -1,49 +1,29 @@
 /* eslint-disable @typescript-eslint/no-use-before-define */
 import React, { useState, useEffect } from "react";
-import { Menu } from "antd";
+import { Menu, Popover } from "antd";
 import { UnregisterCallback, Location } from "history";
+import { getHistory } from "@next-core/brick-kit";
+import { initMenuItemAndMatchCurrentPathKeys } from "@next-libs/basic-components";
 import {
-  SidebarMenuSimpleItem,
-  SidebarMenuGroup,
-  UseBrickConf,
-} from "@next-core/brick-types";
-import { BrickAsComponent, getHistory } from "@next-core/brick-kit";
-import classNames from "classnames";
-import {
-  Link,
-  initMenuItemAndMatchCurrentPathKeys,
-  GeneralIcon,
-} from "@next-libs/basic-components";
+  SidebarMenuItem,
+  SidebarMenuGroups,
+  SidebarMenuSimpleItems,
+  isDivider,
+  isGroup,
+  isSubMenu,
+  renderBrickCom,
+  renderDivider,
+  renderLinkCom,
+  renderSpanCom,
+} from "./utils";
 import style from "./NavMenu.module.css";
-type MenuItemBrick = {
-  useBrick?: UseBrickConf;
-  instanceId?: string;
-  divider?: boolean;
-};
-type SidebarMenuSimpleItems = SidebarMenuSimpleItem & MenuItemBrick;
-type SidebarMenuGroups = SidebarMenuGroup & MenuItemBrick;
-type SidebarMenuItem = SidebarMenuSimpleItems | SidebarMenuGroups;
+import { ThreeLevelMenuPopoverContent } from "./ThreeLevelMenuPopoverContent";
 
 interface SidebarMenuProps {
   menuItems?: SidebarMenuItem[];
   isCustom?: boolean;
   selectedKeys?: string[];
   showTooltip?: boolean;
-}
-
-function isDivider(item: SidebarMenuItem) {
-  return item?.divider;
-}
-
-function isGroup(item: SidebarMenuItem): item is SidebarMenuGroups {
-  return item.type === "group";
-}
-
-function isSubMenu(
-  item: SidebarMenuItem,
-  showMenu?: boolean
-): item is SidebarMenuGroups {
-  return item.type === "subMenu" || (showMenu && item.type === "group");
 }
 
 export function NavMenu(props: SidebarMenuProps): React.ReactElement {
@@ -72,41 +52,6 @@ export function NavMenu(props: SidebarMenuProps): React.ReactElement {
     setSelected();
     return unListen;
   }, []);
-
-  const renderLinkCom = (item: SidebarMenuSimpleItems) => {
-    return (
-      <Link to={item.to} href={item.href} target={item.target}>
-        {/* {item.icon && (
-            <i className={style.menuItemIcon}>
-              <GeneralIcon icon={item.icon} />
-            </i>
-          )} */}
-        <span className={classNames(style.menuText, style.simpleMenuItemText)}>
-          {item.text}
-        </span>
-      </Link>
-    );
-  };
-  const renderSpanCom = (item: SidebarMenuGroups, classNme: string) => {
-    return (
-      <>
-        <span className={classNames(style.menuText, classNme)}>
-          {item.title}
-        </span>
-      </>
-    );
-  };
-  const renderBrickCom = (item: SidebarMenuItem) => {
-    return (
-      <BrickAsComponent useBrick={item.useBrick} data={item}></BrickAsComponent>
-    );
-  };
-
-  const renderDivider = () => {
-    return (
-      <Menu.Divider style={{ height: "1px", margin: "8px 12px 0px 12px" }} />
-    );
-  };
 
   const renderSimpleMenuItem = (
     item: SidebarMenuSimpleItems
@@ -159,13 +104,42 @@ export function NavMenu(props: SidebarMenuProps): React.ReactElement {
     }
   };
 
+  const renderThreeLevelMenu = (item: SidebarMenuGroups): React.ReactNode => {
+    return (
+      <Menu.Item key={item.key} className={style.threeLevelMenuItem}>
+        <Popover
+          zIndex={1060}
+          overlayClassName={style.threeLevelMenuPopover}
+          placement={"bottom"}
+          content={
+            <ThreeLevelMenuPopoverContent
+              menuItem={item}
+              selectedKey={selectedKey}
+            />
+          }
+        >
+          <div
+            className={style.threeLevelBox}
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+          >
+            <div className={style.threeLevelContainer}>
+              {item.useBrick ? renderBrickCom(item) : renderSpanCom(item)}
+            </div>
+          </div>
+        </Popover>
+      </Menu.Item>
+    );
+  };
+
   const renderMenuItem = (
     item: SidebarMenuItem,
-    showSubMenu?: boolean
+    groupAsSubMenu?: boolean
   ): React.ReactNode => {
     return isDivider(item)
       ? renderDivider()
-      : isSubMenu(item, showSubMenu)
+      : isSubMenu(item, groupAsSubMenu)
       ? renderSubMenu(item)
       : isGroup(item)
       ? renderGroupMenu(item)
---
+++ bricks/frame-bricks/src/nav-menu/utils.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import {
+  SidebarMenuSimpleItem,
+  SidebarMenuGroup,
+  UseBrickConf,
+} from "@next-core/brick-types";
+import { Menu } from "antd";
+import { BrickAsComponent } from "@next-core/brick-kit";
+import { Link, GeneralIcon } from "@next-libs/basic-components";
+import style from "./NavMenu.module.css";
+import classNames from "classnames";
+
+type MenuItemBrick = {
+  useBrick?: UseBrickConf;
+  instanceId?: string;
+  divider?: boolean;
+};
+export type SidebarMenuSimpleItems = SidebarMenuSimpleItem & MenuItemBrick;
+export type SidebarMenuGroups = SidebarMenuGroup & MenuItemBrick;
+export type SidebarMenuItem = SidebarMenuSimpleItems | SidebarMenuGroups;
+
+export function isDivider(item: SidebarMenuItem): boolean {
+  return item?.divider;
+}
+
+export const renderDivider = (): React.ReactElement => {
+  return (
+    <Menu.Divider style={{ height: "1px", margin: "8px 12px 0px 12px" }} />
+  );
+};
+
+export function isGroup(item: SidebarMenuItem): item is SidebarMenuGroups {
+  return item.type === "group";
+}
+
+export function isSubMenu(
+  item: SidebarMenuItem,
+  groupAsSubMenu?: boolean
+): item is SidebarMenuGroups {
+  return item.type === "subMenu" || (groupAsSubMenu && item.type === "group");
+}
+
+export const renderBrickCom = (item: SidebarMenuItem): React.ReactElement => {
+  return <BrickAsComponent useBrick={item.useBrick} data={item} />;
+};
+
+export const renderLinkCom = (
+  item: SidebarMenuSimpleItems,
+  classnames?: string | string[],
+  linkStyle?: React.CSSProperties
+): React.ReactElement => {
+  return (
+    <Link to={item.to} href={item.href} target={item.target} style={linkStyle}>
+      {/* {item.icon && (
+          <i className={style.menuItemIcon}>
+            <GeneralIcon icon={item.icon} />
+          </i>
+        )} */}
+      <span className={classNames(classnames)}>{item.text}</span>
+    </Link>
+  );
+};
+
+export const renderSpanCom = (
+  item: SidebarMenuGroups,
+  className?: string
+): React.ReactElement => {
+  return (
+    <>
+      <span className={classNames(style.menuText, className)}>
+        {item.title}
+      </span>
+    </>
+  );
+};
