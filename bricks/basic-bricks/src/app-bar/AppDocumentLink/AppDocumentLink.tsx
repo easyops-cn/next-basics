7649a22d673b42faa4b2092e41dbe58c2bb67efc
annzhang <annzhang@easyops.cn>
2023-08-09 17:09:43 +0800

    feat(basic-bricks): 调整顶部导航栏右侧按钮的样式 Refs NEXT_BUILDER-3697

--- bricks/basic-bricks/src/app-bar/AppDocumentLink/AppDocumentLink.tsx
+++ bricks/basic-bricks/src/app-bar/AppDocumentLink/AppDocumentLink.tsx
@@ -5,17 +5,19 @@ import { useTranslation } from "react-i18next";
 import { getRuntime, useRecentApps } from "@next-core/brick-kit";
 import { Link } from "@next-libs/basic-components";
 import { K, NS_BASIC_BRICKS } from "../../i18n/constants";
-
+import { GeneralButton } from "../../general-button/GeneralButton";
 import styles from "./AppDocumentLink.module.css";
 
 interface AppDocumentLinkProps {
   documentId?: string;
   iconStyle?: React.CSSProperties;
+  isInNavbar?: boolean;
 }
 
 export function AppDocumentLink({
   documentId,
   iconStyle,
+  isInNavbar,
 }: AppDocumentLinkProps): React.ReactElement {
   const { t } = useTranslation(NS_BASIC_BRICKS);
   const { currentApp } = useRecentApps();
@@ -40,7 +42,14 @@ export function AppDocumentLink({
     return null;
   }
 
-  return (
+  return isInNavbar ? (
+    <GeneralButton
+      buttonType="nav"
+      buttonIcon="question-circle"
+      tooltip={t(K.HELP)}
+      buttonUrl={docLink}
+    />
+  ) : (
     <div className={styles.docLinkContainer}>
       <Tooltip title={t(K.HELP)} placement="bottom">
         <Link to={docLink} target="_blank">
