68a4a63ad2dfff368efe6b80bd01e9b9361a59ec
Wang Shenwei <stevewang@easyops.cn>
2023-08-25 18:05:43 +0800

    fix(): catch launchpad error

--- bricks/basic-bricks/src/launchpad-button/LaunchpadButton.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import * as originLanuchpad from "../app-bar/LaunchpadButton/LaunchpadButton";
-
-export function LaunchpadButton(): React.ReactElement {
-  return <originLanuchpad.LaunchpadButton />;
-}
