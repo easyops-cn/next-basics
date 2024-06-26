37bf0e4e91ebe2aaea4cca92b95e7b83c634bb4c
MJmajiong <936233505@qq.com>
2024-06-26 14:34:11 +0800

    fix(): 去掉@next-core/test-next依赖 refs  BRICK-339

--- bricks/forms/test.config.js
+++ bricks/forms/test.config.js
@@ -1,5 +1,3 @@
-// @ts-check
-/** @type {import("@next-core/test-next").TestNextConfig} */
 export default {
   // Todo(steve): Work in progress
   testPathIgnorePatterns: ["<rootDir>/src/property-editors/"],
