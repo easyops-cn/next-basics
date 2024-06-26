e946af7de8c673453de90568bcb9c8a0907df7a4
MJmajiong <936233505@qq.com>
2024-06-26 12:33:01 +0800

    feat(): support formEditor refs BRICK-339

---
+++ bricks/forms/test.config.js
@@ -0,0 +1,7 @@
+// @ts-check
+/** @type {import("@next-core/test-next").TestNextConfig} */
+export default {
+  // Todo(steve): Work in progress
+  testPathIgnorePatterns: ["<rootDir>/src/property-editors/"],
+  coveragePathIgnorePatterns: ["<rootDir>/src/property-editors/"],
+};
