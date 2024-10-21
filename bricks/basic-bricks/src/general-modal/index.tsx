d0e86de967306b76c6b6614f32136537454184ae
alexchen <alexchen@easyops.cn>
2024-10-21 18:12:19 +0800

    fix(): 级联选择如果在弹窗内，点击x会关掉整个弹窗
    Closes INFRA-3463

--- bricks/basic-bricks/src/general-modal/index.tsx
+++ bricks/basic-bricks/src/general-modal/index.tsx
@@ -367,7 +367,10 @@ export class GeneralModalElement extends UpdatingElement {
           nodeName === "div" &&
           clickNode.className.includes("ant-modal-wrap")) ||
         (buttonNode && buttonNode.className.includes("ant-modal-close")) ||
-        clickNode.closest("svg")?.getAttribute("data-icon") === "close-circle"
+        // 如果cascader在modal，点击关闭按钮会误关闭弹窗，这里过滤掉cascader.
+        (clickNode.closest("svg")?.getAttribute("data-icon") ===
+          "close-circle" &&
+          !clickNode.closest(".ant-cascader-picker-clear"))
       ) {
         this.close();
         return;
