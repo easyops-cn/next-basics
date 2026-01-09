f50e35befde5a7b6ba87b10f391ffd12d6344725
zekun <949177145@qq.com>
2026-01-09 16:55:35 +0800

    fix(): 修复英文模式显示key的问题 Refs MONITOR-10614

--- bricks/forms/src/i18n/index.ts
+++ bricks/forms/src/i18n/index.ts
@@ -3,5 +3,9 @@ import { NS_FORMS } from "./constants";
 import en from "./locales/en";
 import zh from "./locales/zh";
 
+i18next.init({
+  //强制所有英语变体都只加载 'en'
+  load: "languageOnly",
+});
 i18next.addResourceBundle("en", NS_FORMS, en);
 i18next.addResourceBundle("zh", NS_FORMS, zh);
